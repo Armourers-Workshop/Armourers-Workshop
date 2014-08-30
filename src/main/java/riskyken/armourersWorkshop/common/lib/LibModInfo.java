e49f3d27fbedfa55a20bf39889d72556f599fd3d
RiskyKen <RiskyKen@users.noreply.github.com>
2014-08-30 18:42:44 +0100

    Build 0.1.0

--- src/main/java/riskyken/armourersWorkshop/common/lib/LibModInfo.java
+++ src/main/java/riskyken/armourersWorkshop/common/lib/LibModInfo.java
@@ -3,7 +3,7 @@ package riskyken.armourersWorkshop.common.lib;
 public class LibModInfo {
     public static final String ID = "armourersWorkshop";
     public static final String NAME = "Armourer's Workshop";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.1.0";
     public static final String CHANNEL = "armsWS";
 
     public static final String PROXY_CLIENT_CLASS = "riskyken.armourersWorkshop.proxies.ClientProxy";
