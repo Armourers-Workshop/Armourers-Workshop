604ff28db49c304a70adf373cb36f604d5230d99
RiskyKen <RiskyKen@users.noreply.github.com>
2014-09-01 20:49:50 +0100

    Changed the way inventory names are localized to improve consistency.

--- src/main/java/riskyken/armourersWorkshop/client/gui/GuiHelper.java
+++ src/main/java/riskyken/armourersWorkshop/client/gui/GuiHelper.java
@@ -2,13 +2,14 @@ package riskyken.armourersWorkshop.client.gui;
 
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.StatCollector;
+import riskyken.armourersWorkshop.common.lib.LibModInfo;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class GuiHelper {
     public static void renderLocalizedGuiName(FontRenderer fontRenderer, int xSize, String name) {
-        String unlocalizedName = "container.inventory." + name + ".name";
+        String unlocalizedName = "inventory." + LibModInfo.ID.toLowerCase() + ":" + name + ".name";
         String localizedName = StatCollector.translateToLocal(unlocalizedName);
         String renderText = unlocalizedName;
         if (!unlocalizedName.equals(localizedName)){
