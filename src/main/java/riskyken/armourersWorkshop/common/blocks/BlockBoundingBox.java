e0f2fcff0ac021f2bcb16cffb412df316c09ece0
RiskyKen <RiskyKen@users.noreply.github.com>
2014-09-03 11:53:40 +0100

    Stopped the player from removing the armourer bounding boxes.

--- src/main/java/riskyken/armourersWorkshop/common/blocks/BlockBoundingBox.java
+++ src/main/java/riskyken/armourersWorkshop/common/blocks/BlockBoundingBox.java
@@ -2,9 +2,16 @@ package riskyken.armourersWorkshop.common.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.particle.EffectRenderer;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
 import riskyken.armourersWorkshop.common.lib.LibBlockNames;
 import riskyken.armourersWorkshop.common.lib.LibModInfo;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockBoundingBox extends Block {
 
@@ -15,6 +22,29 @@ public class BlockBoundingBox extends Block {
         setStepSound(soundTypeCloth);
         setBlockName(LibBlockNames.BOUNDING_BOX);
         setLightOpacity(0);
+    }
+    
+    @Override
+    public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z, boolean willHarvest) {
+        return false;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean addDestroyEffects(World world, int x, int y, int z, int meta, EffectRenderer effectRenderer) {
+        return true;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean addHitEffects(World worldObj, MovingObjectPosition target, EffectRenderer effectRenderer) {
+        return true;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerBlockIcons(IIconRegister register) {
+        blockIcon = register.registerIcon(LibModInfo.ID + ":" + "colourable");
     }
     
     @Override
