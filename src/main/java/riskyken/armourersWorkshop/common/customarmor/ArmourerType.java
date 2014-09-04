bb20fec68a80ea8a311cc4c5b5b3b7db0a4de5a7
RiskyKen <RiskyKen@users.noreply.github.com>
2014-09-04 15:23:24 +0100

    Added armour parts array to each armourer type.

--- src/main/java/riskyken/armourersWorkshop/common/customarmor/ArmourerType.java
+++ src/main/java/riskyken/armourersWorkshop/common/customarmor/ArmourerType.java
@@ -1,12 +1,19 @@
 package riskyken.armourersWorkshop.common.customarmor;
 
 public enum ArmourerType {
-    NONE(-1), HEAD(0), CHEST(1), LEGS(2), SKIRT(2), FEET(3);
-
+    NONE(-1, null),
+    HEAD(0, new ArmourPart[] { ArmourPart.HEAD }),
+    CHEST(1, new ArmourPart[] { ArmourPart.CHEST, ArmourPart.LEFT_ARM, ArmourPart.RIGHT_ARM }),
+    LEGS(2, new ArmourPart[] { ArmourPart.LEFT_LEG, ArmourPart.RIGHT_LEG }),
+    SKIRT(2, new ArmourPart[] { ArmourPart.SKIRT }),
+    FEET(3, new ArmourPart[] { ArmourPart.LEFT_FOOT, ArmourPart.RIGHT_FOOT });
+    
+    private final ArmourPart[] parts;
     private final int slotId;
     
-    private ArmourerType(int slotId) {
+    private ArmourerType(int slotId, ArmourPart[] parts) {
         this.slotId = slotId;
+        this.parts = parts;
     }
     
     public static ArmourerType getOrdinal(int id) {
@@ -15,5 +22,9 @@ public enum ArmourerType {
     
     public int getSlotId() {
         return slotId;
+    }
+    
+    public ArmourPart[] getParts() {
+        return parts;
     }
 }
