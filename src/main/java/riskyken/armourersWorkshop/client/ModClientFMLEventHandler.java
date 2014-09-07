d86f6b902962485127fd33573d883da601397990
RiskyKen <RiskyKen@users.noreply.github.com>
2014-09-07 15:29:10 +0100

    Fixed custom armour model not updating when another custom armour item is swapped with it.

--- src/main/java/riskyken/armourersWorkshop/client/ModClientFMLEventHandler.java
+++ src/main/java/riskyken/armourersWorkshop/client/ModClientFMLEventHandler.java
@@ -1,5 +1,6 @@
 package riskyken.armourersWorkshop.client;
 
+import net.minecraft.item.ItemStack;
 import riskyken.armourersWorkshop.common.items.ItemCustomArmour;
 import riskyken.armourersWorkshop.common.network.PacketHandler;
 import riskyken.armourersWorkshop.common.network.messages.MessageClientArmourUpdate;
@@ -11,22 +12,28 @@ import cpw.mods.fml.relauncher.Side;
 
 public class ModClientFMLEventHandler {
     
-    private boolean[] armourSlots = new boolean[4];
+    private ItemStack[] armourSlots = new ItemStack[4];
     
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
         if (event.side == Side.CLIENT & event.type == Type.PLAYER & event.phase == Phase.END) {
             for (int i = 0; i < 4; i++) {
-                if (armourSlots[i]) {
+                if (armourSlots[i] != null) {
                     if (event.player.getCurrentArmor(i) == null) {
                         armourSlotUpdated(i, false);
-                        armourSlots[i] = false;
+                        armourSlots[i] = null;
+                    } else {
+                        if (!event.player.getCurrentArmor(i).equals(armourSlots[i])) {
+                            armourSlotUpdated(i, false);
+                            armourSlotUpdated(i, true);
+                            armourSlots[i] = event.player.getCurrentArmor(i);
+                        }
                     }
                 } else {
                     if (event.player.getCurrentArmor(i) != null) {
                         if (event.player.getCurrentArmor(i).getItem() instanceof ItemCustomArmour) {
                             armourSlotUpdated(i, true);
-                            armourSlots[i] = true;
+                            armourSlots[i] = event.player.getCurrentArmor(i);
                         }
                     }
                 }
