327dede5a79eb0b30238be625a66db09a4ac8117
RiskyKen <RiskyKen@users.noreply.github.com>
2014-09-08 23:29:40 +0100

    Added HSB colour sliders to the colour mixer.

--- src/main/java/riskyken/armourersWorkshop/client/gui/GuiColourMixer.java
+++ src/main/java/riskyken/armourersWorkshop/client/gui/GuiColourMixer.java
@@ -10,66 +10,26 @@ import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.opengl.GL11;
 
+import riskyken.armourersWorkshop.client.gui.controls.GuiHSBSlider;
+import riskyken.armourersWorkshop.client.gui.controls.GuiHSBSlider.HSBSliderType;
+import riskyken.armourersWorkshop.client.gui.controls.GuiHSBSlider.IHSBSliderCallback;
 import riskyken.armourersWorkshop.common.inventory.ContainerColourMixer;
 import riskyken.armourersWorkshop.common.lib.LibModInfo;
 import riskyken.armourersWorkshop.common.network.PacketHandler;
 import riskyken.armourersWorkshop.common.network.messages.MessageClientGuiColourUpdate;
 import riskyken.armourersWorkshop.common.tileentities.TileEntityColourMixer;
-import cpw.mods.fml.client.config.GuiSlider;
+import riskyken.armourersWorkshop.utils.ModLogger;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class GuiColourMixer extends GuiContainer {
+public class GuiColourMixer extends GuiContainer implements IHSBSliderCallback {
 
     private TileEntityColourMixer tileEntityColourMixer;
     private static final ResourceLocation texture = new ResourceLocation(LibModInfo.ID.toLowerCase(), "textures/gui/colour-mixer.png");
     
-    private GuiSlider redSlider;
-    private GuiSlider greenSlider;
-    private GuiSlider blueSlider;
-    
-    @Override
-    public void initGui() {
-        super.initGui();
-        
-        Color c = new Color(tileEntityColourMixer.getColour());
-        redSlider = new GuiSlider(0, this.guiLeft + 5, this.guiTop + 35, 128 , 20, "R ", "", 0, 255, c.getRed(), false, true);
-        greenSlider = new GuiSlider(1, this.guiLeft + 5, this.guiTop + 60, 128 , 20, "G ", "", 0, 255, c.getGreen(), false, true);
-        blueSlider = new GuiSlider(2, this.guiLeft + 5, this.guiTop + 85, 128 , 20, "B ", "", 0, 255, c.getBlue(), false, true);
-        
-        buttonList.add(redSlider);
-        buttonList.add(greenSlider);
-        buttonList.add(blueSlider);
-    }
-    
-    private void checkForColourUpdates() {
-        if (tileEntityColourMixer.getHasItemUpdateAndReset()) {
-            Color c = new Color(tileEntityColourMixer.getColour());
-            redSlider.setValue(c.getRed());
-            greenSlider.setValue(c.getGreen());
-            blueSlider.setValue(c.getBlue());
-            redSlider.updateSlider();
-            greenSlider.updateSlider();
-            blueSlider.updateSlider();
-        }
-    }
-    
-    @Override
-    protected void mouseMovedOrUp(int mouseX, int mouseY, int which) {
-        super.mouseMovedOrUp(mouseX, mouseY, which);
-        
-        if (which != 0) { return; }
-        
-        Color colourNew = new Color(redSlider.getValueInt(), greenSlider.getValueInt(), blueSlider.getValueInt());
-        Color colourOld = new Color(tileEntityColourMixer.getColour());
-        
-        if (colourNew.equals(colourOld)) { return; }
-        
-        PacketHandler.networkWrapper.sendToServer(new MessageClientGuiColourUpdate(colourNew.getRGB(), false));
-    }
-    
-    
+    private Color colour;
+    private GuiHSBSlider[] slidersHSB;
     
     public GuiColourMixer(InventoryPlayer invPlayer, TileEntityColourMixer tileEntityColourMixer) {
         super(new ContainerColourMixer(invPlayer, tileEntityColourMixer));
@@ -79,9 +39,53 @@ public class GuiColourMixer extends GuiContainer {
     }
     
     @Override
+    public void initGui() {
+        super.initGui();
+        colour = new Color(tileEntityColourMixer.getColour());
+        float[] hsbvals = Color.RGBtoHSB(colour.getRed(), colour.getGreen(), colour.getBlue(), null);
+        slidersHSB = new GuiHSBSlider[3];
+        slidersHSB[0] = new GuiHSBSlider(0, this.guiLeft + 5, this.guiTop + 30, 128, 10, this, HSBSliderType.HUE, hsbvals[0], hsbvals[0], hsbvals[2]);
+        slidersHSB[1] = new GuiHSBSlider(1, this.guiLeft + 5, this.guiTop + 50, 128, 10, this, HSBSliderType.SATURATION, hsbvals[1], hsbvals[0], hsbvals[2]);
+        slidersHSB[2] = new GuiHSBSlider(2, this.guiLeft + 5, this.guiTop + 70, 128, 10, this, HSBSliderType.BRIGHTNESS, hsbvals[2], hsbvals[0], hsbvals[2]);
+        buttonList.add(slidersHSB[0]);
+        buttonList.add(slidersHSB[1]);
+        buttonList.add(slidersHSB[2]);
+    }
+    
+    private void checkForColourUpdates() {
+        if (tileEntityColourMixer.getHasItemUpdateAndReset()) {
+            ModLogger.log("colour update");
+            Color c = new Color(tileEntityColourMixer.getColour());
+            float[] hsbvals = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
+            slidersHSB[0].setValue(hsbvals[0]);
+            slidersHSB[1].setValue(hsbvals[1]);
+            slidersHSB[2].setValue(hsbvals[2]);
+        }
+    }
+    
+    @Override
+    protected void mouseMovedOrUp(int mouseX, int mouseY, int which) {
+        super.mouseMovedOrUp(mouseX, mouseY, which);
+        if (which != 0) { return; }
+        float[] hsbvals = { (float)slidersHSB[0].getValue(), (float)slidersHSB[1].getValue(), (float)slidersHSB[2].getValue() };
+        Color colourNew = Color.getHSBColor(hsbvals[0], hsbvals[1], hsbvals[2]);
+        Color colourOld = new Color(tileEntityColourMixer.getColour());
+        if (colourNew.equals(colourOld)) { return; }
+        PacketHandler.networkWrapper.sendToServer(new MessageClientGuiColourUpdate(colourNew.getRGB(), false));
+    }
+    
+    @Override
     protected void drawGuiContainerForegroundLayer(int p_146979_1_, int p_146979_2_) {
         GuiHelper.renderLocalizedGuiName(this.fontRendererObj, this.xSize, tileEntityColourMixer.getInventoryName());
         this.fontRendererObj.drawString(I18n.format("container.inventory", new Object[0]), 8, this.ySize - 96 + 2, 4210752);
+        
+        String labelHue = GuiHelper.getLocalizedControlName(tileEntityColourMixer.getInventoryName(), "label.hue");
+        String labelSaturation = GuiHelper.getLocalizedControlName(tileEntityColourMixer.getInventoryName(), "label.saturation");
+        String labelBrightness = GuiHelper.getLocalizedControlName(tileEntityColourMixer.getInventoryName(), "label.brightness");
+        
+        this.fontRendererObj.drawString(labelHue, 5, 22, 4210752);
+        this.fontRendererObj.drawString(labelSaturation, 5, 42, 4210752);
+        this.fontRendererObj.drawString(labelBrightness, 5, 62, 4210752);
     }
     
     @Override
@@ -91,12 +95,25 @@ public class GuiColourMixer extends GuiContainer {
         Minecraft.getMinecraft().getTextureManager().bindTexture(texture);
         drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
         
-        float red = (float) redSlider.getValueInt() / 255;
-        float green = (float) greenSlider.getValueInt() / 255;
-        float blue = (float) blueSlider.getValueInt() / 255;
+        float red = (float) colour.getRed() / 255;
+        float green = (float) colour.getGreen() / 255;
+        float blue = (float) colour.getBlue() / 255;
         GL11.glColor4f(red, green, blue, 1F);
         
         drawTexturedModalRect(this.guiLeft + 146, this.guiTop + 59, 146, 59, 12, 13);
         GL11.glColor4f(1F, 1F, 1F, 1F);
+    }
+
+    @Override
+    public void valueUpdated(GuiHSBSlider source, double sliderValue) {
+        float[] hsbvals = { (float)slidersHSB[0].getValue(), (float)slidersHSB[1].getValue(), (float)slidersHSB[2].getValue() };
+        hsbvals[source.getType().ordinal()] = (float)sliderValue;
+        this.colour = Color.getHSBColor(hsbvals[0], hsbvals[1], hsbvals[2]);
+        if (source.getType() == HSBSliderType.HUE) {
+            slidersHSB[1].setHue((float) source.getValue());
+        }
+        if (source.getType() == HSBSliderType.BRIGHTNESS) {
+            slidersHSB[1].setBrightness((float) source.getValue());
+        }
     }
 }
---
+++ src/main/java/riskyken/armourersWorkshop/client/gui/controls/GuiHSBSlider.java
@@ -0,0 +1,148 @@
+package riskyken.armourersWorkshop.client.gui.controls;
+
+import java.awt.Color;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
+
+import riskyken.armourersWorkshop.common.lib.LibModInfo;
+import cpw.mods.fml.client.config.GuiSlider;
+import cpw.mods.fml.client.config.GuiUtils;
+
+public class GuiHSBSlider extends GuiSlider {
+
+    private static final ResourceLocation sliderTexture = new ResourceLocation(LibModInfo.ID.toLowerCase(), "textures/gui/controls/sliderHue.png");
+    
+    private HSBSliderType type;
+    private IHSBSliderCallback callback = null;
+    private float hueValue;
+    private float briValue;
+    
+    public GuiHSBSlider(int id, int xPos, int yPos, int width, int height, IHSBSliderCallback callback, HSBSliderType type, float curValue, float hueValue, float brightnessValue) {
+        super(id, xPos, yPos, width, height, "", "", 0, 256 * 6 - 5, 0, false, false);
+        this.type = type;
+        this.hueValue = hueValue;
+        this.briValue = brightnessValue;
+        setValue(curValue);
+        this.callback = callback;
+    }
+    
+    @Override
+    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        if (this.visible) {
+            this.field_146123_n = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
+            int k = this.getHoverState(this.field_146123_n);
+            GuiUtils.drawContinuousTexturedBox(buttonTextures, this.xPosition, this.yPosition, 0, 46 + k * 20, this.width, this.height, 200, 20, 2, 3, 2, 2, this.zLevel);
+            mc.renderEngine.bindTexture(sliderTexture);
+            
+            if (type == HSBSliderType.SATURATION) {
+                Color hueColour = Color.getHSBColor(hueValue, 1F, 1F);
+                float red = (float) hueColour.getRed() / 255;
+                float green = (float) hueColour.getGreen() / 255;
+                float blue = (float) hueColour.getBlue() / 255;
+                GL11.glColor4f(red, green, blue, 1.0F);
+                drawTexturedModalRectScaled(xPosition + 1, yPosition + 1, 0, 176, 256, 20, this.width - 2, this.height - 2);
+            }
+            
+            int srcY = 236;
+            
+            if (type == HSBSliderType.BRIGHTNESS) {
+                srcY -= 20;
+            }
+            if (type == HSBSliderType.SATURATION) {
+                srcY -= 40;
+            }
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            if (type == HSBSliderType.SATURATION) {
+                Color hueColour = Color.getHSBColor(0F, 0F, briValue);
+                float red = (float) hueColour.getRed() / 255;
+                float green = (float) hueColour.getGreen() / 255;
+                float blue = (float) hueColour.getBlue() / 255;
+                GL11.glColor4f(red, green, blue, 1.0F);
+                drawTexturedModalRectScaled(xPosition + 1, yPosition + 1, 0, srcY, 231, 20, this.width - 2, this.height - 2);
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            } else {
+                drawTexturedModalRectScaled(xPosition + 1, yPosition + 1, 0, srcY, 256, 20, this.width - 2, this.height - 2);
+            }
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            
+            this.mouseDragged(mc, mouseX, mouseY);
+        }
+    }
+    
+    @Override
+    protected void mouseDragged(Minecraft mc, int par2, int par3) {
+        if (this.visible) {
+            if (this.dragging) {
+                this.sliderValue = (par2 - (this.xPosition + 4)) / (float)(this.width - 8);
+                updateSlider();
+                if (callback != null) {
+                    callback.valueUpdated(this, this.sliderValue);
+                }
+            }
+
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            
+            ScaledResolution screenRes = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
+            double scaleWidth = (double)mc.displayWidth / screenRes.getScaledWidth_double();
+            double scaleHeight = (double)mc.displayHeight / screenRes.getScaledHeight_double();
+            
+            GL11.glEnable(GL11.GL_SCISSOR_TEST);
+            GL11.glScissor((int) ((this.xPosition + 1) * scaleWidth),  (mc.displayHeight) - (int)((this.yPosition + height - 1) * scaleHeight), (int) ((width - 2) * scaleWidth), (int) ((height - 2) * scaleHeight));
+            
+            this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (float)(this.width - 3) - 2), this.yPosition, 0, 0, 7, 4);
+            this.drawTexturedModalRect(this.xPosition + (int)(this.sliderValue * (float)(this.width - 3) - 2), this.yPosition + this.height - 4, 7, 0, 7, 4);
+            GL11.glDisable(GL11.GL_SCISSOR_TEST);
+        }
+    }
+    
+    public HSBSliderType getType() {
+        return type;
+    }
+    
+    @Override
+    public double getValue() {
+        return sliderValue;
+    }
+    
+    @Override
+    public void setValue(double value) {
+        this.sliderValue = value;
+        updateSlider();
+        if (callback != null) {
+            callback.valueUpdated(this, this.sliderValue);
+        }
+    }
+    
+    public void setHue(float value) {
+        this.hueValue = value;
+    }
+    
+    public void setBrightness(float value) {
+        this.briValue = value;
+    }
+    
+    public void drawTexturedModalRectScaled (int x, int y, int u, int v, int srcWidth, int srcHeight, int tarWidth, int tarHeight) {
+        float f = 0.00390625F;
+        float f1 = 0.00390625F;
+        Tessellator tessellator = Tessellator.instance;
+        tessellator.startDrawingQuads();
+        tessellator.addVertexWithUV((double)(x + 0), (double)(y + tarHeight), (double)this.zLevel, (double)((float)(u + 0) * f), (double)((float)(v + srcHeight) * f1));
+        tessellator.addVertexWithUV((double)(x + tarWidth), (double)(y + tarHeight), (double)this.zLevel, (double)((float)(u + srcWidth) * f), (double)((float)(v + srcHeight) * f1));
+        tessellator.addVertexWithUV((double)(x + tarWidth), (double)(y + 0), (double)this.zLevel, (double)((float)(u + srcWidth) * f), (double)((float)(v + 0) * f1));
+        tessellator.addVertexWithUV((double)(x + 0), (double)(y + 0), (double)this.zLevel, (double)((float)(u + 0) * f), (double)((float)(v + 0) * f1));
+        tessellator.draw();
+    }
+    
+    public enum HSBSliderType {
+        HUE, SATURATION, BRIGHTNESS;
+    }
+    
+    public interface IHSBSliderCallback {
+        public void valueUpdated(GuiHSBSlider source, double sliderValue);
+    }
+}
