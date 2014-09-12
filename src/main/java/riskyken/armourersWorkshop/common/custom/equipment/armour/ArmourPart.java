4de0d0bc6d3e239729dc6e7c19c345f9150d8e65
RiskyKen <RiskyKen@users.noreply.github.com>
2014-09-12 11:50:33 +0100

    Allowed arm armour to be wider.

--- src/main/java/riskyken/armourersWorkshop/common/custom/equipment/armour/ArmourPart.java
+++ src/main/java/riskyken/armourersWorkshop/common/custom/equipment/armour/ArmourPart.java
@@ -1,23 +1,59 @@
 package riskyken.armourersWorkshop.common.custom.equipment.armour;
 
 public enum ArmourPart {
-    HEAD(1, 0, 1, 20, 20, 20, 11, 1, 11),
+    HEAD(
+            1, 0, 1,
+            20, 20, 20,
+            11, 1, 11
+            ),
     
-    CHEST(4, 0, 1, 14, 14, 10, 11, 13, 6),
+    CHEST(
+            4, 0, 1,
+            14, 14, 10,
+            11, 13, 6
+            ),
     
-    LEFT_ARM(2, 0, 11, 8, 18, 10, 7, 11, 16),
+    LEFT_ARM(
+            1, 0, 11,
+            9, 18, 10,
+            7, 11, 16
+            ),
     
-    RIGHT_ARM(12, 0, 11, 8, 18, 10, 15, 11, 16),
+    RIGHT_ARM(
+            12, 0, 11,
+            9, 18, 10,
+            15, 11, 16
+            ),
     
-    LEFT_LEG(2, 5, 7, 8, 9, 8, 6, 14, 11),
+    LEFT_LEG(
+            2, 5, 7,
+            8, 9, 8,
+            6, 14, 11
+            ),
     
-    RIGHT_LEG(12, 5, 7, 8, 9, 8, 16, 14, 11),
+    RIGHT_LEG(
+            12, 5, 7,
+            8, 9, 8,
+            16, 14, 11
+            ),
     
-    SKIRT(1, 0, 1, 20, 13, 20, 11, 14, 11),
+    SKIRT(
+            1, 0, 1,
+            20, 14, 20,
+            11, 14, 11
+            ),
     
-    LEFT_FOOT(2, 0, 6, 8, 5, 10, 6, 14, 11),
+    LEFT_FOOT(
+            2, 0, 6,
+            8, 5, 10,
+            6, 14, 11
+            ),
     
-    RIGHT_FOOT(12, 0, 6, 8, 5, 10, 16, 14, 11);
+    RIGHT_FOOT(
+            12, 0, 6,
+            8, 5, 10,
+            16, 14, 11
+            );
     
     private final int xOffset;
     private final int yOffset;
