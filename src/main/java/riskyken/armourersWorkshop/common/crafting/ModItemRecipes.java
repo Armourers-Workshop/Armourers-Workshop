092ed73502697a2578dfafcb866ad30509c28c73
RiskyKen <RiskyKen@users.noreply.github.com>
2014-09-30 10:54:42 +0100

    Added recipes for all items.

--- src/main/java/riskyken/armourersWorkshop/common/crafting/ModItemRecipes.java
+++ src/main/java/riskyken/armourersWorkshop/common/crafting/ModItemRecipes.java
@@ -3,6 +3,7 @@ package riskyken.armourersWorkshop.common.crafting;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import riskyken.armourersWorkshop.common.blocks.ModBlocks;
 import riskyken.armourersWorkshop.common.items.ModItems;
 
 public final class ModItemRecipes {
@@ -13,7 +14,7 @@ public final class ModItemRecipes {
             "  w",
             " i ",
             "s  ",
-            'w', Blocks.wool,
+            'w', ModBlocks.colourable,
             'i', Items.iron_ingot,
             's', "stickWood"});
         
@@ -21,8 +22,61 @@ public final class ModItemRecipes {
             " w ",
             " iw",
             "s  ",
-            'w', Blocks.wool,
+            'w', ModBlocks.colourable,
             'i', Items.iron_ingot,
             's', "stickWood"});
+        
+        CraftingManager.addShapedRecipe(new ItemStack(ModItems.colourPicker, 1, 0), new Object[] {
+            " lg",
+            "lwl",
+            "ll ",
+            'w', ModBlocks.colourable,
+            'g', Blocks.glass,
+            'l', Items.leather});
+        
+        CraftingManager.addShapedRecipe(new ItemStack(ModItems.burnTool, 1, 0), new Object[] {
+            " wd",
+            " iw",
+            "s  ",
+            'w', ModBlocks.colourable,
+            'i', Items.iron_ingot,
+            'd', "dyeBlack",
+            's', "stickWood"});
+        
+        CraftingManager.addShapedRecipe(new ItemStack(ModItems.dodgeTool, 1, 0), new Object[] {
+            " wd",
+            " iw",
+            "s  ",
+            'w', ModBlocks.colourable,
+            'i', Items.iron_ingot,
+            'd', "dyeWhite",
+            's', "stickWood"});
+        
+        CraftingManager.addShapedRecipe(new ItemStack(ModItems.shadeNoiseTool, 1, 0), new Object[] {
+            " wd",
+            " iw",
+            "s  ",
+            'w', ModBlocks.colourable,
+            'i', Items.iron_ingot,
+            'd', Blocks.cobblestone,
+            's', "stickWood"});
+        
+        CraftingManager.addShapedRecipe(new ItemStack(ModItems.colourNoiseTool, 1, 0), new Object[] {
+            " wd",
+            " iw",
+            "s  ",
+            'w', ModBlocks.colourable,
+            'i', Items.iron_ingot,
+            'd', Blocks.mossy_cobblestone,
+            's', "stickWood"});
+        
+        CraftingManager.addShapedRecipe(new ItemStack(ModItems.equipmentSkinTemplate, 8, 0), new Object[] {
+            "cc",
+            "cc",
+            'c', ModBlocks.colourable});
+        
+        CraftingManager.addShapelessRecipe(new ItemStack(ModItems.guideBook, 1, 0), new Object[] {
+            new ItemStack(Items.book, 1),
+            new ItemStack(ModBlocks.colourable, 1)});
     }
 }
