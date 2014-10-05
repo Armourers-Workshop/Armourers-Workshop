aa3e04bc744b2b2a6178d13747dfe06c339e2b56
RiskyKen <RiskyKen@users.noreply.github.com>
2014-10-05 13:22:40 +0100

    Added some comments to the API.

--- src/main/java/riskyken/armourersWorkshop/api/common/customEquipment/EquipmentManager.java
+++ src/main/java/riskyken/armourersWorkshop/api/common/customEquipment/EquipmentManager.java
@@ -9,20 +9,43 @@ import riskyken.armourersWorkshop.api.common.event.RemoveEntityEquipmentEvent;
 
 public class EquipmentManager {
     
+    /**
+     * Adds custom equipment data to an entity.
+     * Note: addCustomEquipmentToEntity(Entity, EnumArmourType, int) is more performance friendly.
+     * @param entity Entity to add the custom equipment to.
+     * @param equipmentStack ItemStack that has the equipment id.
+     */
     public static void addCustomEquipmentToEntity(Entity entity, ItemStack equipmentStack) {
         int equipmentId = EquipmentNBTHelper.getEquipmentIdFromStack(equipmentStack);
         EnumArmourType armourType = GetEquipmentTypeEvent.call(equipmentId);
-        addCustomEquipmentToEntity(entity, armourType, equipmentId);
+        if (armourType != EnumArmourType.NONE) {
+            addCustomEquipmentToEntity(entity, armourType, equipmentId);
+        }
     }
     
+    /**
+     * Adds custom equipment data to an entity.
+     * @param entity Entity to add the custom equipment to.
+     * @param armourType Armour type to add.
+     * @param equipmentId Equipment id to add.
+     */
     public static void addCustomEquipmentToEntity(Entity entity, EnumArmourType armourType, int equipmentId) {
         AddEntityEquipmentEvent.call(entity, armourType, equipmentId);
     }
     
+    /**
+     * Removes custom equipment data from an entity.
+     * @param entity Entity to remove the custom equipment from.
+     * @param armourType The type of equipment to remove.
+     */
     public static void removeCustomEquipmentFromEntity(Entity entity, EnumArmourType armourType) {
         RemoveEntityEquipmentEvent.call(entity, armourType);
     }
     
+    /**
+     * Removes all custom equipment data from an entity.
+     * @param entity Entity to remove the custom equipment from.
+     */
     public static void removeAllCustomEquipmentFromEntity(Entity entity) {
         RemoveEntityEquipmentEvent.call(entity, EnumArmourType.HEAD);
         RemoveEntityEquipmentEvent.call(entity, EnumArmourType.CHEST);
