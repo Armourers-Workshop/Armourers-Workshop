ab8b43e77db3ae8024be148e577559af2bf3e1c8
RiskyKen <RiskyKen@users.noreply.github.com>
2014-11-15 17:58:24 +0000

    Added a mod prefix to the debug text on the F3 screen.

--- src/main/java/riskyken/armourersWorkshop/client/ModForgeEventHandler.java
+++ src/main/java/riskyken/armourersWorkshop/client/ModForgeEventHandler.java
@@ -1,8 +1,10 @@
 package riskyken.armourersWorkshop.client;
 
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import riskyken.armourersWorkshop.ArmourersWorkshop;
 import riskyken.armourersWorkshop.client.render.EquipmentItemRenderCache;
+import riskyken.armourersWorkshop.common.lib.LibModInfo;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -14,8 +16,10 @@ public class ModForgeEventHandler {
     public void onDebugText(RenderGameOverlayEvent.Text event) {
         if (event.left != null && event.left.size() > 0) {
             event.left.add("");
-            event.left.add("Item model cache size:  " + EquipmentItemRenderCache.getCacheSize());
-            event.left.add("Player model cache size:  " + ArmourersWorkshop.proxy.getPlayerModelCacheSize());
+            String prefix = EnumChatFormatting.GOLD + "[" + LibModInfo.NAME + "] " + EnumChatFormatting.WHITE;
+            
+            event.left.add(prefix + "Item model cache size:  " + EquipmentItemRenderCache.getCacheSize());
+            event.left.add(prefix + "Player model cache size:  " + ArmourersWorkshop.proxy.getPlayerModelCacheSize());
         }
     }
 }
