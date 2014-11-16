d3f899cf9a77ff467837e0992d5bdfbb891c037b
RiskyKen <RiskyKen@users.noreply.github.com>
2014-11-16 12:52:17 +0000

    Rename block to stop conflicts when updating.

--- src/main/java/riskyken/armourersWorkshop/common/lib/LibBlockNames.java
+++ src/main/java/riskyken/armourersWorkshop/common/lib/LibBlockNames.java
@@ -1,9 +1,9 @@
 package riskyken.armourersWorkshop.common.lib;
 
 public class LibBlockNames {
-    public static final String ARMOURER_BRAIN = "armourerBrain";
+    public static final String ARMOURER_BRAIN = "armourer";
     public static final String ARMOUR_LIBRARY = "armourLibrary";
-    public static final String BOUNDING_BOX = "boundingBox";
+    public static final String BOUNDING_BOX = "awBoundingBox";
     public static final String COLOURABLE = "colourable";
     public static final String COLOURABLE_GLOWING = "colourableGlowing";
     public static final String COLOUR_MIXER = "colourMixer";
