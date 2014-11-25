4e51f1b94abb42e4334f6334492bfd6cbe5919a4
RiskyKen <RiskyKen@users.noreply.github.com>
2014-11-25 18:09:27 +0000

    Added shift clicking to the mannequin GUI.

--- src/main/java/riskyken/armourersWorkshop/common/inventory/ContainerMannequin.java
+++ src/main/java/riskyken/armourersWorkshop/common/inventory/ContainerMannequin.java
@@ -6,6 +6,7 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import riskyken.armourersWorkshop.api.common.equipment.EnumEquipmentType;
+import riskyken.armourersWorkshop.common.items.ItemEquipmentSkin;
 import riskyken.armourersWorkshop.common.tileentities.TileEntityMannequin;
 
 public class ContainerMannequin extends Container {
@@ -30,7 +31,69 @@ public class ContainerMannequin extends Container {
     }
     
     @Override
-    public ItemStack transferStackInSlot(EntityPlayer player, int slotID) {
+    public ItemStack transferStackInSlot(EntityPlayer player, int slotId) {
+        Slot slot = getSlot(slotId);
+        if (slot != null && slot.getHasStack()) {
+            ItemStack stack = slot.getStack();
+            ItemStack result = stack.copy();
+
+            if (slotId < 6) {
+                if (!this.mergeItemStack(stack, 15, 42, false)) {
+                    if (!this.mergeItemStack(stack, 6, 15, false)) {
+                        return null;
+                    }
+                }
+            } else {
+                if (stack.getItem() instanceof ItemEquipmentSkin) {
+                    switch (stack.getItemDamage()) {
+                    case 0:
+                        if (!this.mergeItemStack(stack, 0, 1, false)) {
+                            return null;
+                        }
+                        break;
+                    case 1:
+                        if (!this.mergeItemStack(stack, 1, 2, false)) {
+                            return null;
+                        }
+                        break;
+                    case 2:
+                        if (!this.mergeItemStack(stack, 2, 3, false)) {
+                            return null;
+                        }
+                        break;
+                    case 3:
+                        if (!this.mergeItemStack(stack, 3, 4, false)) {
+                            return null;
+                        }
+                        break;
+                    case 4:
+                        if (!this.mergeItemStack(stack, 4, 5, false)) {
+                            return null;
+                        }
+                        break;
+                    case 5:
+                        if (!this.mergeItemStack(stack, 5, 6, false)) {
+                            return null;
+                        }
+                        break;
+                    default:
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            }
+
+            if (stack.stackSize == 0) {
+                slot.putStack(null);
+            } else {
+                slot.onSlotChanged();
+            }
+
+            slot.onPickupFromSlot(player, stack);
+
+            return result;
+        }
         return null;
     }
     
