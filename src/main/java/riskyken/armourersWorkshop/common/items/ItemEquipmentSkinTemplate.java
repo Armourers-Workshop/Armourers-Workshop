07623fc90bb108e351f42c91268993a764193e91
RiskyKen <RiskyKen@users.noreply.github.com>
2014-12-01 17:31:10 +0000

    Updated gift.

--- src/main/java/riskyken/armourersWorkshop/common/items/ItemEquipmentSkinTemplate.java
+++ src/main/java/riskyken/armourersWorkshop/common/items/ItemEquipmentSkinTemplate.java
@@ -2,18 +2,22 @@ package riskyken.armourersWorkshop.common.items;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTUtil;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
+import riskyken.armourersWorkshop.common.blocks.ModBlocks;
 import riskyken.armourersWorkshop.common.lib.LibItemNames;
 import riskyken.armourersWorkshop.common.lib.LibModInfo;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemEquipmentSkinTemplate extends AbstractModItem {
+    
+    private static final String TAG_OWNER = "owner";
     
     public ItemEquipmentSkinTemplate() {
         super(LibItemNames.EQUIPMENT_SKIN_TEMPLATE);
@@ -35,10 +39,12 @@ public class ItemEquipmentSkinTemplate extends AbstractModItem {
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
         if (!world.isRemote) {
             if (stack.getItemDamage() == 1000) {
-                ItemStack[] gifts = { new ItemStack(Items.cake, 1), new ItemStack(Items.golden_apple, 1),
-                        new ItemStack(Items.painting, 1)};
-                
-                if (player.inventory.addItemStackToInventory(gifts[world.rand.nextInt(gifts.length)].copy())) {
+                ItemStack giftStack = new ItemStack(ModBlocks.doll, 1);
+                NBTTagCompound profileTag = new NBTTagCompound();
+                NBTUtil.func_152460_a(profileTag, player.getGameProfile());
+                giftStack.setTagCompound(new NBTTagCompound());
+                giftStack.getTagCompound().setTag(TAG_OWNER, profileTag);
+                if (player.inventory.addItemStackToInventory(giftStack)) {
                     stack.stackSize--;
                 } else {
                     player.addChatComponentMessage(new ChatComponentText(StatCollector.translateToLocal("chat.armourersworkshop:inventoryFull")));
