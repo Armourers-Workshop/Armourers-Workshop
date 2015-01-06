dd1c310cb70c084af6c7c5bbe729417c61adf35a
RiskyKen <RiskyKen@users.noreply.github.com>
2015-01-06 22:04:13 +0000

    Increased the length of weapon skins by 8 blocks.

--- src/main/java/riskyken/armourersWorkshop/api/common/equipment/EnumEquipmentPart.java
+++ src/main/java/riskyken/armourersWorkshop/api/common/equipment/EnumEquipmentPart.java
@@ -71,7 +71,7 @@ public enum EnumEquipmentPart {
     WEAPON(
             4, 4, 12,
             -2, -2, -2,
-            new int[] {18, 28, 8, 0, 8, 8},
+            new int[] {18, 28, 16, 8, 8, 8},
             0, 18, 0,
             null),
     BOW(
