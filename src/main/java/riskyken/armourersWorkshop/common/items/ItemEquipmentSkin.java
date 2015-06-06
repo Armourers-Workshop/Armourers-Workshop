0772a9017f599c8fef8a7e44f0f1d4e7d8c28088
RiskyKen <RiskyKen@users.noreply.github.com>
2015-06-06 12:38:02 +0100

    Added skin support for BuildCraft robots. Closes #28

--- src/main/java/riskyken/armourersWorkshop/common/items/ItemEquipmentSkin.java
+++ src/main/java/riskyken/armourersWorkshop/common/items/ItemEquipmentSkin.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IIcon;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
 
 import riskyken.armourersWorkshop.api.common.skin.type.ISkinType;
+import riskyken.armourersWorkshop.client.handler.EquipmentRenderHandler;
 import riskyken.armourersWorkshop.client.lib.LibItemResources;
 import riskyken.armourersWorkshop.client.model.ClientModelCache;
 import riskyken.armourersWorkshop.client.settings.Keybindings;
@@ -25,10 +28,13 @@ import riskyken.armourersWorkshop.common.skin.data.SkinPointer;
 import riskyken.armourersWorkshop.common.skin.type.SkinTypeRegistry;
 import riskyken.armourersWorkshop.utils.EquipmentNBTHelper;
 import riskyken.armourersWorkshop.utils.TranslateUtils;
+import buildcraft.api.robots.IRobotOverlayItem;
+import cpw.mods.fml.common.Optional;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemEquipmentSkin extends AbstractModItem {
+@Optional.Interface(iface = "buildcraft.api.robots.IRobotOverlayItem", modid = "BuildCraft|Core")
+public class ItemEquipmentSkin extends AbstractModItem implements IRobotOverlayItem {
 
     public ItemEquipmentSkin() {
         super(LibItemNames.EQUIPMENT_SKIN, false);
@@ -138,5 +144,31 @@ public class ItemEquipmentSkin extends AbstractModItem {
         }
         
         return this.itemIcon;
+    }
+
+    @Optional.Method(modid = "BuildCraft|Core")
+    @Override
+    public boolean isValidRobotOverlay(ItemStack stack) {
+        if (!EquipmentNBTHelper.stackHasSkinData(stack)) {
+            return false;
+        }
+        SkinPointer skinPointer = EquipmentNBTHelper.getSkinPointerFromStack(stack);
+        if (skinPointer.getSkinType() != SkinTypeRegistry.skinHead) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Optional.Method(modid = "BuildCraft|Core")
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void renderRobotOverlay(ItemStack stack, TextureManager textureManager) {
+        GL11.glPushMatrix();
+        GL11.glScalef(1.0125F, 1.0125F, 1.0125F);
+        GL11.glTranslatef(0.0F, -0.25F, 0.0F);
+        GL11.glRotatef(180F, 0F, 0F, 1F);
+        GL11.glRotatef(-90F, 0F, 1F, 0F);
+        EquipmentRenderHandler.INSTANCE.renderSkinWithHelper(stack);
+        GL11.glPopMatrix();
     }
 }
