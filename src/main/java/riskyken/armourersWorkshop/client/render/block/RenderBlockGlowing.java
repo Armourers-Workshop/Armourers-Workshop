0d24777d1f3dd634a1df1638ecf49f0d7a39a94d
RiskyKen <RiskyKen@users.noreply.github.com>
2015-09-11 20:44:04 +0100

    Fixed equipment cube textures in inventory.

--- src/main/java/riskyken/armourersWorkshop/client/render/block/RenderBlockGlowing.java
+++ src/main/java/riskyken/armourersWorkshop/client/render/block/RenderBlockGlowing.java
@@ -32,32 +32,32 @@ public class RenderBlockGlowing implements ISimpleBlockRenderingHandler {
         
         tessellator.startDrawingQuads();
         tessellator.setNormal(0.0F, -1F, 0.0F);
-        renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+        renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(255, metadata));
         tessellator.draw();
 
         tessellator.startDrawingQuads();
         tessellator.setNormal(0.0F, 1.0F, 0.0F);
-        renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+        renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(255, metadata));
         tessellator.draw();
 
         tessellator.startDrawingQuads();
         tessellator.setNormal(0.0F, 0.0F, -1F);
-        renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+        renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(255, metadata));
         tessellator.draw();
         
         tessellator.startDrawingQuads();
         tessellator.setNormal(0.0F, 0.0F, 1.0F);
-        renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+        renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(255, metadata));
         tessellator.draw();
         
         tessellator.startDrawingQuads();
         tessellator.setNormal(-1F, 0.0F, 0.0F);
-        renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+        renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(255, metadata));
         tessellator.draw();
         
         tessellator.startDrawingQuads();
         tessellator.setNormal(1.0F, 0.0F, 0.0F);
-        renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, metadata));
+        renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(255, metadata));
         tessellator.draw();
         
         GL11.glTranslatef(0.5F, 0.5F, 0.5F);
