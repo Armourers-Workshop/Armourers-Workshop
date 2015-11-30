3d9c3c7ee2273df6b7f6e2b3f297e0a379ba5ecd
RiskyKen <RiskyKen@users.noreply.github.com>
2015-11-30 12:34:49 +0000

    Improved render speed of paint markers.

--- src/main/java/riskyken/armourersWorkshop/client/render/tileEntity/RenderBlockColourable.java
+++ src/main/java/riskyken/armourersWorkshop/client/render/tileEntity/RenderBlockColourable.java
@@ -39,47 +39,44 @@ public class RenderBlockColourable extends TileEntitySpecialRenderer {
     }
     
     public void renderTileEntityAt(TileEntityColourable tileEntity, double x, double y, double z, float partialTickTime) {
-        ICubeColour cubeColour = tileEntity.getColour();
-        for (int i = 0; i < 6; i++) {
-            ForgeDirection dir = ForgeDirection.getOrientation(i);
-            int paintType = cubeColour.getPaintType(i) & 0xFF;
-            if (paintType != 255) {
-                ModRenderHelper.disableLighting();
-                
-                GL11.glDisable(GL11.GL_LIGHTING);
-                bindTexture(MARKERS);
-                GL11.glColor3f(0.77F, 0.77F, 0.77F);
-                PaintType pt = PaintType.getPaintTypeFromUKey(paintType);
-                
-                
-                
-                if (lastWorldTimeUpdate != tileEntity.getWorldObj().getTotalWorldTime()) {
-                    lastWorldTimeUpdate = tileEntity.getWorldObj().getTotalWorldTime();
-                    if (isPlayerHoldingPaintingTool()) {
-                        markerAlpha += 0.25F;
-                        if (markerAlpha > 1F) {
-                            markerAlpha = 1F;
-                        }
-                    } else {
-                        markerAlpha -= 0.25F;
-                        if (markerAlpha < 0F) {
-                            markerAlpha = 0F;
-                        }
-                    }
+        if (lastWorldTimeUpdate != tileEntity.getWorldObj().getTotalWorldTime()) {
+            lastWorldTimeUpdate = tileEntity.getWorldObj().getTotalWorldTime();
+            if (isPlayerHoldingPaintingTool()) {
+                markerAlpha += 0.25F;
+                if (markerAlpha > 1F) {
+                    markerAlpha = 1F;
                 }
-                
-                if (markerAlpha > 0F) {
-                    ModRenderHelper.enableAlphaBlend();
-                    GL11.glColor4f(0.77F, 0.77F, 0.77F, markerAlpha);
-                    renderFaceWithMarker(x, y, z, dir, pt.ordinal());
-                    ModRenderHelper.disableAlphaBlend();
+            } else {
+                markerAlpha -= 0.25F;
+                if (markerAlpha < 0F) {
+                    markerAlpha = 0F;
                 }
-                
-                GL11.glColor4f(1F, 1F, 1F, 1F);
-                ModRenderHelper.enableLighting();
-                RenderHelper.enableStandardItemLighting();
             }
         }
+        
+        ICubeColour cubeColour = tileEntity.getColour();
+        //ModRenderHelper.disableLighting();
+        GL11.glDisable(GL11.GL_LIGHTING);
+        ModRenderHelper.enableAlphaBlend();
+        renderer.startDrawingQuads();
+        renderer.setColourRGBA_F(0.7F, 0.7F, 0.7F, markerAlpha);
+        if (markerAlpha > 0F) {
+            for (int i = 0; i < 6; i++) {
+                ForgeDirection dir = ForgeDirection.getOrientation(i);
+                int paintType = cubeColour.getPaintType(i) & 0xFF;
+                if (paintType != 255) {
+                    bindTexture(MARKERS);
+                    GL11.glColor3f(0.77F, 0.77F, 0.77F);
+                    PaintType pt = PaintType.getPaintTypeFromUKey(paintType);
+                    renderFaceWithMarker(x, y, z, dir, pt.ordinal());
+                }
+            }
+        }
+        renderer.draw();
+        GL11.glColor4f(1F, 1F, 1F, 1F);
+        ModRenderHelper.disableAlphaBlend();
+        ModRenderHelper.enableLighting();
+        RenderHelper.enableStandardItemLighting();
     }
     
     private void renderFaceWithMarker(double x, double y, double z, ForgeDirection face, int marker) {
@@ -93,52 +90,52 @@ public class RenderBlockColourable extends TileEntitySpecialRenderer {
         float offset = 0.001F;
         switch (face) {
         case DOWN:
-            renderer.startDrawingQuads();
+            //renderer.startDrawingQuads();
             renderer.addVertexWithUV(x, y - offset, z, xStart, yEnd);
             renderer.addVertexWithUV(x + 1F, y - offset, z, xEnd, yEnd);
             renderer.addVertexWithUV(x + 1F, y - offset, z + 1F, xEnd, yStart);
             renderer.addVertexWithUV(x, y - offset, z + 1F, xStart, yStart);
-            renderer.draw();
+            //renderer.draw();
             break;
         case UP:
-            renderer.startDrawingQuads();
+            //renderer.startDrawingQuads();
             renderer.addVertexWithUV(x, y + 1F + offset, z + 1F, xStart, yEnd);
             renderer.addVertexWithUV(x + 1F, y + 1F + offset, z + 1F, xEnd, yEnd);
             renderer.addVertexWithUV(x + 1F, y + 1F + offset, z, xEnd, yStart);
             renderer.addVertexWithUV(x, y + 1F + offset, z, xStart, yStart);
-            renderer.draw();
+            //renderer.draw();
             break;
         case NORTH:
-            renderer.startDrawingQuads();
+            //renderer.startDrawingQuads();
             renderer.addVertexWithUV(x + 1F, y, z - offset, xStart, yEnd);
             renderer.addVertexWithUV(x, y, z - offset, xEnd, yEnd);
             renderer.addVertexWithUV(x, y + 1F, z - offset, xEnd, yStart);
             renderer.addVertexWithUV(x + 1F, y + 1F, z - offset, xStart, yStart);
-            renderer.draw();
+            //renderer.draw();
             break;
         case SOUTH:
-            renderer.startDrawingQuads();
+            //renderer.startDrawingQuads();
             renderer.addVertexWithUV(x, y, z + 1F + offset, xStart, yEnd);
             renderer.addVertexWithUV(x + 1F, y, z + 1F + offset, xEnd, yEnd);
             renderer.addVertexWithUV(x + 1F, y + 1F, z + 1F + offset, xEnd, yStart);
             renderer.addVertexWithUV(x, y + 1F, z + 1F + offset, xStart, yStart);
-            renderer.draw();
+            //renderer.draw();
             break;
         case WEST:
-            renderer.startDrawingQuads();
+            //renderer.startDrawingQuads();
             renderer.addVertexWithUV(x - offset, y, z , xStart, yEnd);
             renderer.addVertexWithUV(x - offset, y, z + 1F, xEnd, yEnd);
             renderer.addVertexWithUV(x - offset, y + 1F, z + 1F, xEnd, yStart);
             renderer.addVertexWithUV(x - offset, y + 1F, z, xStart, yStart);
-            renderer.draw();
+            //renderer.draw();
             break;
         case EAST:
-            renderer.startDrawingQuads();
+            //renderer.startDrawingQuads();
             renderer.addVertexWithUV(x + 1 + offset, y, z + 1F, xStart, yEnd);
             renderer.addVertexWithUV(x + 1 + offset, y, z, xEnd, yEnd);
             renderer.addVertexWithUV(x + 1 + offset, y + 1F, z, xEnd, yStart);
             renderer.addVertexWithUV(x + 1 + offset, y + 1F, z + 1F, xStart, yStart);
-            renderer.draw();
+            //renderer.draw();
             break;
         default:
             break;
