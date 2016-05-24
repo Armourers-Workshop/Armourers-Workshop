a519eebfc2f7e63551a8de4b557d55ed905f1a15
RiskyKen <RiskyKen@users.noreply.github.com>
2016-05-24 23:31:37 +0100

    Cleaned up F3 debug text

--- src/main/java/riskyken/armourersWorkshop/client/handler/DebugTextHandler.java
+++ src/main/java/riskyken/armourersWorkshop/client/handler/DebugTextHandler.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.gui.GuiPlayerInfo;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import riskyken.armourersWorkshop.ArmourersWorkshop;
@@ -32,27 +33,28 @@ public class DebugTextHandler {
             List playerList = localPlayer.sendQueue.playerInfoList;
             event.left.add("");
             event.left.add(EnumChatFormatting.GOLD + "[" + LibModInfo.NAME + "]");
-            String dataLine = "";
-            dataLine += "sc:" + ArmourersWorkshop.proxy.getPlayerModelCacheSize() + " ";
-            dataLine += "pc:" + ClientSkinCache.INSTANCE.getPartCount() + " ";
-            dataLine += "mc:" + ClientSkinCache.INSTANCE.getModelCount() + " ";
-            dataLine += "pd:" + SkinModelRenderer.INSTANCE.getSkinDataMapSize() + " ";
-            event.left.add(dataLine);
-            dataLine = "bq:" + ModelBakery.INSTANCE.getBakingQueueSize() + " ";
-            dataLine += "rq:" + ClientSkinCache.INSTANCE.getRequestQueueSize() + " ";
-            dataLine += "sr:" + ModClientFMLEventHandler.skinRenderLastTick + " ";
-            dataLine += "tc:" + ClientSkinPaintCache.INSTANCE.size() + " ";
-            if (!Minecraft.getMinecraft().isIntegratedServerRunning()) {
-                for (int i = 0; i < playerList.size(); i++) {
-                    GuiPlayerInfo player = (GuiPlayerInfo) playerList.get(i);
-                    if (player.name.equals(localPlayer.getCommandSenderName())) {
-                        dataLine += " ping:" + player.responseTime + "ms";
-                        break;
-                    }
-                } 
+            event.left.add("Skins Rendered: " + ModClientFMLEventHandler.skinRenderLastTick);
+            event.left.add("Model Count: " + ClientSkinCache.INSTANCE.getModelCount());
+            if (GuiScreen.isCtrlKeyDown()) {
+                event.left.add("Skin Count: " + ArmourersWorkshop.proxy.getPlayerModelCacheSize());
+                event.left.add("Part Count: " + ClientSkinCache.INSTANCE.getPartCount());
+                event.left.add("Player Data: " + SkinModelRenderer.INSTANCE.getSkinDataMapSize());
+                int bakeQueue = ModelBakery.INSTANCE.getBakingQueueSize();
+                event.left.add("Baking Queue: " + bakeQueue);
+                event.left.add("Request Queue: " + (ClientSkinCache.INSTANCE.getRequestQueueSize() - bakeQueue));
+                event.left.add("Texture Count: " + ClientSkinPaintCache.INSTANCE.size());
+                if (!Minecraft.getMinecraft().isIntegratedServerRunning()) {
+                    for (int i = 0; i < playerList.size(); i++) {
+                        GuiPlayerInfo player = (GuiPlayerInfo) playerList.get(i);
+                        if (player.name.equals(localPlayer.getCommandSenderName())) {
+                            event.left.add("ping:" + player.responseTime + "ms");
+                            break;
+                        }
+                    } 
+                }
+            } else {
+                event.left.add("Hold " + EnumChatFormatting.GREEN + "Ctrl" + EnumChatFormatting.WHITE + " for more.");  
             }
-            
-            event.left.add(dataLine);
         }
     }
 }
