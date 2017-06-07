e68cb661a846ca4a98b6d7e98fb1db2dfba167fc
RiskyKen <RiskyKen@users.noreply.github.com>
2017-06-07 10:07:24 +0100

    added library class for GUI resource

---
+++ src/main/java/riskyken/armourersWorkshop/client/lib/LibGuiResources.java
@@ -0,0 +1,13 @@
+package riskyken.armourersWorkshop.client.lib;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import riskyken.armourersWorkshop.common.lib.LibModInfo;
+
+@SideOnly(Side.CLIENT)
+public class LibGuiResources {
+    
+    private static final String PREFIX_RESOURCE = LibModInfo.ID.toLowerCase() + ":textures/gui/";
+    
+
+}
