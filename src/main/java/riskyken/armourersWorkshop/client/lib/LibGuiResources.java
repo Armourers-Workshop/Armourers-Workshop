221efd5af050ea546f6976532c6f9ab6968b7919
RiskyKen <RiskyKen@users.noreply.github.com>
2017-06-29 20:16:24 +0100

    Removed old armourer texture.

--- src/main/java/riskyken/armourersWorkshop/client/lib/LibGuiResources.java
+++ src/main/java/riskyken/armourersWorkshop/client/lib/LibGuiResources.java
@@ -9,6 +9,6 @@ public class LibGuiResources {
     
     private static final String PREFIX_RESOURCE = LibModInfo.ID.toLowerCase() + ":textures/gui/";
     
-    public static final String ARMOURER = PREFIX_RESOURCE + "armourerNew.png";
+    public static final String ARMOURER = PREFIX_RESOURCE + "armourer.png";
     public static final String ARMOURER_TABS = PREFIX_RESOURCE + "armourerTabs.png";
 }
