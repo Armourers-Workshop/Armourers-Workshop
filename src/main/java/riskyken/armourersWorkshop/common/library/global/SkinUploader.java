222adbe4ff1869bfd0b7f573419601eae1c597ac
RiskyKen <RiskyKen@users.noreply.github.com>
2017-08-18 11:08:26 +0100

    Updated skin uploader to handle more post data.

--- src/main/java/riskyken/armourersWorkshop/common/library/global/SkinUploader.java
+++ src/main/java/riskyken/armourersWorkshop/common/library/global/SkinUploader.java
@@ -1,14 +1,16 @@
 package riskyken.armourersWorkshop.common.library.global;
 
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
 
 import net.minecraft.entity.player.EntityPlayer;
@@ -17,14 +19,28 @@ import riskyken.armourersWorkshop.utils.ModLogger;
 
 public final class SkinUploader implements Runnable {
     
+    private static final String UPLOAD_URL = "https://plushie.moe/armourers_workshop/user-skin-upload.php";
+    
+    private File file;
+    private String name;
+    private String userId;
+    private String description;
+    private String accessToken;
+    
     private EntityPlayer player;
     
-    private SkinUploader(EntityPlayer player) {
-        this.player = player;
+    
+    
+    private SkinUploader(File file, String name, String userId, String description, String accessToken) {
+        this.file = file;
+        this.name = name;
+        this.userId = userId;
+        this.description = description;
+        this.accessToken = accessToken;
     }
     
-    public static void startUpload(EntityPlayer player) {
-        (new Thread(new SkinUploader(player), LibModInfo.NAME + " upload thread.")).start();
+    public static void startUpload(File file, String name, String userId, String description, String accessToken) {
+        (new Thread(new SkinUploader(file, name, userId, description, accessToken), LibModInfo.NAME + " upload thread.")).start();
     }
     
     @Override
@@ -36,83 +52,87 @@ public final class SkinUploader implements Runnable {
         }
     }
     
-    private void uploadSkin() throws IOException{
-        ModLogger.log("Upload Test Started");
-        
-        if (player == null) {
-            return;
-        }
-        if (player.getGameProfile() == null) {
-            return;
-        }
-        if (player.getGameProfile().getId() == null) {
-            return;
-        }
-        
-        /*
-        HttpPost post = new HttpPost();
-        FileEntity s = new FileEntity(null);
-        //org.apache.http.entity.ContentType.MULTIPART_FORM_DATA
-        //org.apache.http.entity.ByteArrayEntity
-        post.setEntity(s);
-        
-        HttpClient client = new DefaultHttpClient();
-        HttpResponse response = client.execute(post);
-        */
-        
-        
-        //http://plushie.moe/armourers_workshop/skin-list.php
-        //http://plushie.moe/armourers_workshop/skin-upload.php
-        //fileToUpload
-        String skinUpload = "https://plushie.moe/armourers_workshop/skin-upload.php";
+    private void writeMultipartData(PrintWriter writer, String boundary, String name, String value) {
         String CRLF = "\r\n";
         String charset = "UTF-8";
         
-        //skinUpload = "http://localhost:8080/";
+        writer.append("--" + boundary).append(CRLF);
+        writer.append("Content-Disposition: form-data; name=\"" + name + "\"").append(CRLF);
+        writer.append("Content-Type: text/plain; charset=").append(charset).append(CRLF);
+        writer.append(CRLF);
+        writer.append(value).append(CRLF);
+    }
+    
+    private void writeMultipartFile(PrintWriter writer, OutputStream output, String boundary, String name, String filename, byte[] fileData) throws IOException {
+        String CRLF = "\r\n";
+        String charset = "UTF-8";
+        
+        writer.append("--" + boundary).append(CRLF);
+        writer.append("Content-Disposition: form-data; name=\"" + name + "\"; filename=\"" + filename + "\"").append(CRLF);
+        writer.append("Content-Type: application/octet-stream").append(CRLF);
+        writer.append("Content-Transfer-.Encoding: binary").append(CRLF);
+        writer.append(CRLF).flush();
+        output.write(fileData);
+        output.flush(); // Important before continuing with writer!
+    }
+    
+    private void uploadSkin() throws IOException{
+        ModLogger.log("Upload Test Started");
+        
+        String CRLF = "\r\n";
+        String charset = "UTF-8";
         
         String boundary = Long.toHexString(System.currentTimeMillis());
-        String fileName = player.getGameProfile().getName() + ".txt";
-        String fileData = player.getGameProfile().getId().toString();
         
-        URL uploadUrl = new URL(skinUpload);
+        URL uploadUrl = new URL(UPLOAD_URL);
         URLConnection connection = uploadUrl.openConnection();
         
         connection.setDoOutput(true);
         connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
         OutputStream output = null;
         PrintWriter writer = null;
+        FileInputStream fileInputStream = new FileInputStream(file);
+        byte[] fileBytes = IOUtils.toByteArray(fileInputStream);
+        fileInputStream.close();
         
         try {
             output = connection.getOutputStream();
             writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
             
+            //Send forum data
+            writeMultipartData(writer, boundary, "name", name);
+            writeMultipartData(writer, boundary, "userId", userId);
+            writeMultipartData(writer, boundary, "description", description);
+            writeMultipartData(writer, boundary, "accessToken", accessToken);
+            
             // Send binary file.
             writer.append("--" + boundary).append(CRLF);
-            writer.append("Content-Disposition: form-data; name=\"fileToUpload\"; filename=\"" + fileName + "\"").append(CRLF);
+            writer.append("Content-Disposition: form-data; name=\"fileToUpload\"; filename=\"" + name + "\"").append(CRLF);
             writer.append("Content-Type: application/octet-stream").append(CRLF);
             writer.append("Content-Transfer-.Encoding: binary").append(CRLF);
             writer.append(CRLF).flush();
-            output.write(fileData.getBytes());
-            //Files.copy(binaryFile.toPath(), output);
+            output.write(fileBytes);
             output.flush(); // Important before continuing with writer!
+            
+            
             writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
-            
-            
             // End of multipart/form-data.
             writer.append("--" + boundary + "--").append(CRLF).flush();
             
             
-            
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            
-            String data = "";
-            
-            String line = "";
-            while ((line = reader.readLine()) != null) {
-                data += line;
+            String result = "";
+            InputStream inputStream = null;
+            try {
+                inputStream = connection.getInputStream();
+                result = IOUtils.toString(inputStream, Charsets.UTF_8);
+            } catch (IOException e) {
+                IOUtils.closeQuietly(inputStream);
+                e.printStackTrace();
+            } finally {
+                IOUtils.closeQuietly(inputStream);
             }
             
-            ModLogger.log(data);
+            ModLogger.log(result);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
