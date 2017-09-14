8b74b425156e459710e8e9d1e7f066227f19f91c
RiskyKen <RiskyKen@users.noreply.github.com>
2017-09-14 09:51:46 +0100

    Fixed the invalid java version error not showing if the user had an older version of java 8.

--- src/main/java/riskyken/armourersWorkshop/common/library/global/GlobalSkinLibraryUtils.java
+++ src/main/java/riskyken/armourersWorkshop/common/library/global/GlobalSkinLibraryUtils.java
@@ -122,7 +122,10 @@ public final class GlobalSkinLibraryUtils {
     }
     
     public static boolean isValidJavaVersion(int[] javaVersion) {
-        if (javaVersion[0] < 8 & javaVersion[1] < 101) {
+        if (javaVersion[0] < 8) {
+            return false;
+        }
+        if (javaVersion[1] < 101) {
             return false;
         }
         return true;
