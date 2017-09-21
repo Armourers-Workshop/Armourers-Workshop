79cb46e7b5b23cd5bd5cef18455adae0bb8cfb2f
RiskyKen <RiskyKen@users.noreply.github.com>
2017-09-21 14:55:43 +0100

    Created test code for setting skins on a spawning entity.

--- src/main/java/riskyken/armourersWorkshop/common/skin/entity/EntitySkinHandler.java
+++ src/main/java/riskyken/armourersWorkshop/common/skin/entity/EntitySkinHandler.java
@@ -2,6 +2,7 @@ package riskyken.armourersWorkshop.common.skin.entity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
@@ -10,14 +11,24 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import riskyken.armourersWorkshop.ArmourersWorkshop;
 import riskyken.armourersWorkshop.api.common.skin.entity.IEntitySkinHandler;
 import riskyken.armourersWorkshop.api.common.skin.entity.ISkinnableEntity;
+import riskyken.armourersWorkshop.api.common.skin.type.ISkinType;
+import riskyken.armourersWorkshop.common.library.ILibraryManager;
+import riskyken.armourersWorkshop.common.library.LibraryFile;
+import riskyken.armourersWorkshop.common.library.LibraryFileList;
 import riskyken.armourersWorkshop.common.skin.EntityEquipmentData;
+import riskyken.armourersWorkshop.common.skin.cache.CommonSkinCache;
+import riskyken.armourersWorkshop.common.skin.data.Skin;
 import riskyken.armourersWorkshop.utils.ModLogger;
+import riskyken.armourersWorkshop.utils.SkinIOUtils;
+import riskyken.armourersWorkshop.utils.SkinNBTHelper;
 
 public final class EntitySkinHandler implements IEntitySkinHandler {
     
@@ -91,7 +102,56 @@ public final class EntitySkinHandler implements IEntitySkinHandler {
         if (isValidEntity(event.entity)) {
             ISkinnableEntity skinnableEntity = entityMap.get(event.entity.getClass());
             ExPropsEntityEquipmentData.register(event.entity, skinnableEntity);
+            //giveRandomSkin(event.entity);
         }
+    }
+    
+    private void giveRandomSkin(Entity entity) {
+        ExPropsEntityEquipmentData entityEquipmentData = ExPropsEntityEquipmentData.getExtendedPropsForEntity(entity);
+        if (entityEquipmentData == null) {
+            return;
+        }
+        
+        ArrayList<ISkinType> skinTypes = entityEquipmentData.getSkinInventory().getSkinTypes();
+        for (int i = 0; i < skinTypes.size(); i++) {
+            ISkinType skinType = skinTypes.get(i);
+            LibraryFile libraryFile = getRandomSkinOfType(skinType);
+            if (libraryFile == null) {
+                continue;
+            }
+            
+            Skin skin = SkinIOUtils.loadSkinFromFileName(libraryFile.getFullName() + SkinIOUtils.SKIN_FILE_EXTENSION);
+            if (skin == null) {
+                continue;
+            }
+            skin.getProperties().setProperty(Skin.KEY_FILE_NAME, libraryFile.getFullName() + SkinIOUtils.SKIN_FILE_EXTENSION);
+            CommonSkinCache.INSTANCE.addEquipmentDataToCache(skin, libraryFile);
+            
+            ItemStack skinStack = SkinNBTHelper.makeEquipmentSkinStack(skin);
+            if (skinStack == null) {
+                continue;
+            }
+            entityEquipmentData.getSkinInventory().setInventorySlotContents(i, skinStack);
+        }
+    }
+    
+    public LibraryFile getRandomSkinOfType(ISkinType skinType) {
+        ILibraryManager libraryManager = ArmourersWorkshop.getProxy().libraryManager;
+        LibraryFileList fileList = libraryManager.getClientPublicFileList();
+        
+        ArrayList<LibraryFile> typeList = new ArrayList<LibraryFile>();
+        
+        for (int i = 0; i < fileList.getFileCount(); i++) {
+            LibraryFile libraryFile = fileList.getFileList().get(i);
+            if (libraryFile.skinType == skinType) {
+                typeList.add(libraryFile);
+            }
+        }
+        Random random = new Random();
+        if (!typeList.isEmpty()) {
+            return typeList.get(random.nextInt(typeList.size()));
+        }
+        return null;
     }
     
     @SideOnly(Side.CLIENT)
