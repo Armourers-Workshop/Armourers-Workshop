154bc90c80188054924a77117f86077e8cd9cce3
RiskyKen <RiskyKen@users.noreply.github.com>
2018-02-22 12:27:56 +0000

    Updated Botania add-on for new tools.

--- src/main/java/riskyken/armourersWorkshop/common/addons/AddonBotania.java
+++ src/main/java/riskyken/armourersWorkshop/common/addons/AddonBotania.java
@@ -9,8 +9,19 @@ public class AddonBotania extends ModAddon {
     @Override
     public void preInit() {
         addItemOverride(ItemOverrideType.SWORD, "manasteelSword");
-        addItemOverride(ItemOverrideType.SWORD, "terraSword");
         addItemOverride(ItemOverrideType.SWORD, "elementiumSword");
-        addItemOverride(ItemOverrideType.SWORD, "excaliber");
+        addItemOverride(ItemOverrideType.SWORD, "terraSword");
+        addItemOverride(ItemOverrideType.SWORD, "starSword");
+        
+        addItemOverride(ItemOverrideType.PICKAXE, "manasteelPick");
+        addItemOverride(ItemOverrideType.PICKAXE, "elementiumPick");
+        addItemOverride(ItemOverrideType.PICKAXE, "terraPick");
+        
+        addItemOverride(ItemOverrideType.AXE, "manasteelAxe");
+        addItemOverride(ItemOverrideType.AXE, "elementiumAxe");
+        addItemOverride(ItemOverrideType.AXE, "terraAxe");
+        
+        addItemOverride(ItemOverrideType.SHOVEL, "manasteelShovel");
+        addItemOverride(ItemOverrideType.SHOVEL, "elementiumShovel");
     }
 }
