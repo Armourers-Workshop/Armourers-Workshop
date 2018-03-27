22a2e6cfa35d4fee190e7a21e29c8c575021a62c
RiskyKen <RiskyKen@users.noreply.github.com>
2018-03-27 23:11:00 +0100

    Fixed global library render bounding box.

--- src/main/java/riskyken/armourersWorkshop/common/tileentities/TileEntityGlobalSkinLibrary.java
+++ src/main/java/riskyken/armourersWorkshop/common/tileentities/TileEntityGlobalSkinLibrary.java
@@ -1,12 +1,14 @@
 package riskyken.armourersWorkshop.common.tileentities;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import riskyken.armourersWorkshop.common.network.messages.client.MessageClientGuiButton.IButtonPress;
 
 public class TileEntityGlobalSkinLibrary extends TileEntity implements IButtonPress  {
     
     public TileEntityGlobalSkinLibrary() {
-        // TODO Auto-generated constructor stub
     }
 
     @Override
@@ -19,5 +21,11 @@ public class TileEntityGlobalSkinLibrary extends TileEntity implements IButtonPr
         if (buttonId == 0) {
             
         }
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public AxisAlignedBB getRenderBoundingBox() {
+        return AxisAlignedBB.getBoundingBox(xCoord, yCoord + 1, zCoord, xCoord + 1, yCoord + 2, zCoord + 1);
     }
 }
