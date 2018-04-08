4ec43e66f7b83875c9bb6d9905f31bb0732ea36e
RiskyKen <RiskyKen@users.noreply.github.com>
2018-04-09 00:18:19 +0100

    Changed skin uploader to use the multipart forum data class.

--- src/main/java/riskyken/armourersWorkshop/common/library/global/MultipartForm.java
+++ src/main/java/riskyken/armourersWorkshop/common/library/global/MultipartForm.java
@@ -73,7 +73,7 @@ public class MultipartForm {
             writer.append("Content-Disposition: form-data; name=\"" + name + "\"; filename=\"" + filename + "\"").append(CRLF);
             writer.append("Content-Type: application/octet-stream").append(CRLF);
             writer.append("Content-Transfer-.Encoding: binary").append(CRLF);
-            writer.append(CRLF);
+            writer.append(CRLF).flush();;
             output.write(fileBytes);
             output.flush(); // Important must flush before continuing with writer!
             writer.append(CRLF).flush();
--- src/main/java/riskyken/armourersWorkshop/common/library/global/SkinUploader.java
+++ src/main/java/riskyken/armourersWorkshop/common/library/global/SkinUploader.java
@@ -1,19 +1,10 @@
 package riskyken.armourersWorkshop.common.library.global;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
-
-import org.apache.commons.io.Charsets;
-import org.apache.commons.io.IOUtils;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -63,89 +54,17 @@ public final class SkinUploader {
         }
     }
     
-    private static void writeMultipartData(PrintWriter writer, String boundary, String name, String value) {
-        String CRLF = "\r\n";
-        String charset = "UTF-8";
+    private static String doSkinUpload(byte[] file, String name, String userId, String description, String accessToken) throws IOException {
         
-        writer.append("--" + boundary).append(CRLF);
-        writer.append("Content-Disposition: form-data; name=\"" + name + "\"").append(CRLF);
-        writer.append("Content-Type: text/plain; charset=").append(charset).append(CRLF);
-        writer.append(CRLF);
-        writer.append(value).append(CRLF);
-    }
-    
-    private static void writeMultipartFile(PrintWriter writer, OutputStream output, String boundary, String name, String filename, byte[] fileData) throws IOException {
-        String CRLF = "\r\n";
-        String charset = "UTF-8";
+        ModLogger.log("Upload Test Started " + "name" + name +  "userId" + userId +  "description" + description +  "accessToken" + accessToken);
+        MultipartForm multipartForm = new MultipartForm(UPLOAD_URL);
         
-        writer.append("--" + boundary).append(CRLF);
-        writer.append("Content-Disposition: form-data; name=\"" + name + "\"; filename=\"" + filename + "\"").append(CRLF);
-        writer.append("Content-Type: application/octet-stream").append(CRLF);
-        writer.append("Content-Transfer-.Encoding: binary").append(CRLF);
-        writer.append(CRLF).flush();
-        output.write(fileData);
-        output.flush(); // Important before continuing with writer!
-    }
-    
-    private static String doSkinUpload(byte[] file, String name, String userId, String description, String accessToken) throws IOException{
-        ModLogger.log("Upload Test Started");
+        multipartForm.addText("name", name);
+        multipartForm.addText("userId", userId);
+        multipartForm.addText("description", description);
+        multipartForm.addText("accessToken", accessToken);
+        multipartForm.addFile("fileToUpload", name, file);
         
-        String CRLF = "\r\n";
-        String charset = "UTF-8";
-        
-        String boundary = Long.toHexString(System.currentTimeMillis());
-        
-        URL uploadUrl = new URL(UPLOAD_URL);
-        URLConnection connection = uploadUrl.openConnection();
-        
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-        OutputStream output = null;
-        PrintWriter writer = null;
-        
-        String result = "";
-        
-        try {
-            output = connection.getOutputStream();
-            writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
-            
-            //Send forum data
-            writeMultipartData(writer, boundary, "name", name);
-            writeMultipartData(writer, boundary, "userId", userId);
-            writeMultipartData(writer, boundary, "description", description);
-            writeMultipartData(writer, boundary, "accessToken", accessToken);
-            
-            // Send binary file.
-            writer.append("--" + boundary).append(CRLF);
-            writer.append("Content-Disposition: form-data; name=\"fileToUpload\"; filename=\"" + name + "\"").append(CRLF);
-            writer.append("Content-Type: application/octet-stream").append(CRLF);
-            writer.append("Content-Transfer-.Encoding: binary").append(CRLF);
-            writer.append(CRLF).flush();
-            output.write(file);
-            output.flush(); // Important before continuing with writer!
-            
-            
-            writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
-            // End of multipart/form-data.
-            writer.append("--" + boundary + "--").append(CRLF).flush();
-            
-            
-            InputStream inputStream = null;
-            try {
-                inputStream = connection.getInputStream();
-                result = IOUtils.toString(inputStream, Charsets.UTF_8);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                IOUtils.closeQuietly(inputStream);
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            IOUtils.closeQuietly(writer);
-            IOUtils.closeQuietly(output);
-        }
-        return result;
+        return multipartForm.upload();
     }
 }
