c2b3612cd98d05c8cbddee6ac0218535a047aca9
RiskyKen <RiskyKen@users.noreply.github.com>
2018-10-05 12:38:52 +0100

    Fixed development version warning message not showing.

--- src/main/java/moe/plushie/armourers_workshop/client/handler/ModClientFMLEventHandler.java
+++ src/main/java/moe/plushie/armourers_workshop/client/handler/ModClientFMLEventHandler.java
@@ -2,8 +2,19 @@ package moe.plushie.armourers_workshop.client.handler;
 
 import moe.plushie.armourers_workshop.client.settings.Keybindings;
 import moe.plushie.armourers_workshop.common.config.ConfigHandler;
+import moe.plushie.armourers_workshop.common.config.ConfigHandlerClient;
+import moe.plushie.armourers_workshop.common.lib.LibModInfo;
 import moe.plushie.armourers_workshop.common.network.PacketHandler;
 import moe.plushie.armourers_workshop.common.network.messages.client.MessageClientKeyPress;
+import moe.plushie.armourers_workshop.common.update.UpdateCheck;
+import moe.plushie.armourers_workshop.utils.TranslateUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -19,36 +30,36 @@ public class ModClientFMLEventHandler {
     public static float renderTickTime;
     public static int skinRendersThisTick = 0;
     public static int skinRenderLastTick = 0;
-    /*
+    
     @SubscribeEvent
     public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent eventArgs) {
-        if (eventArgs.modID.equals(LibModInfo.ID)) {
+        if (eventArgs.getModID().equals(LibModInfo.ID)) {
             ConfigHandler.loadConfigFile();
             ConfigHandlerClient.loadConfigFile();
         }
     }
     
     public void onPlayerTickEndEvent() {
-        EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
+        EntityPlayerSP player = Minecraft.getMinecraft().player;
         if (!shownUpdateInfo && UpdateCheck.updateFound) {
             shownUpdateInfo = true;
-            ChatComponentText updateMessage = new ChatComponentText(TranslateUtils.translate("chat.armourersworkshop:updateAvailable", UpdateCheck.remoteModVersion) + " ");
-            ChatComponentText updateURL = new ChatComponentText(TranslateUtils.translate("chat.armourersworkshop:updateDownload"));
-            updateURL.getChatStyle().setUnderlined(true);
-            updateURL.getChatStyle().setColor(EnumChatFormatting.BLUE);
-            updateURL.getChatStyle().setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(TranslateUtils.translate("chat.armourersworkshop:updateDownloadRollover"))));
-            updateURL.getChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, LibModInfo.DOWNLOAD_URL));
+            TextComponentString updateMessage = new TextComponentString(TranslateUtils.translate("chat.armourers_workshop:updateAvailable", UpdateCheck.remoteModVersion) + " ");
+            TextComponentString updateURL = new TextComponentString(TranslateUtils.translate("chat.armourers_workshop:updateDownload"));
+            updateURL.getStyle().setUnderlined(true);
+            updateURL.getStyle().setColor(TextFormatting.BLUE);
+            updateURL.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString(TranslateUtils.translate("chat.armourers_workshop:updateDownloadRollover"))));
+            updateURL.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, LibModInfo.DOWNLOAD_URL));
             updateMessage.appendSibling(updateURL);
-            player.addChatMessage(updateMessage);
+            player.sendMessage(updateMessage);
         }
         if (!showmDevWarning && LibModInfo.DEVELOPMENT_VERSION) {
-            ChatComponentText devWarning = new ChatComponentText(TranslateUtils.translate("chat.armourersworkshop:devWarning"));
-            devWarning.getChatStyle().setColor(EnumChatFormatting.RED);
-            player.addChatMessage(devWarning);
+            TextComponentString devWarning = new TextComponentString(TranslateUtils.translate("chat.armourers_workshop:devWarning"));
+            devWarning.getStyle().setColor(TextFormatting.RED);
+            player.sendMessage(devWarning);
             showmDevWarning = true;
         }
     }
-    */
+    
     @SubscribeEvent
     public void onKeyInputEvent(InputEvent.KeyInputEvent event) {
         if (Keybindings.openCustomArmourGui.isPressed() & ConfigHandler.allowEquipmentWardrobe) {
@@ -64,7 +75,7 @@ public class ModClientFMLEventHandler {
         if (event.side == Side.CLIENT) {
             if (event.type == Type.PLAYER) {
                 if (event.phase == Phase.END) {
-                    //onPlayerTickEndEvent();
+                    onPlayerTickEndEvent();
                 }
             }
         }
