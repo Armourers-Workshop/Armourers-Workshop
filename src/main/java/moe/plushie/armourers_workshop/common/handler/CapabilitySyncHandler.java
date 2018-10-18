1ef52cc0f856af8dbff32a976a6a9390438dcdb4
RiskyKen <RiskyKen@users.noreply.github.com>
2018-10-18 22:34:28 +0100

    Fixed skins not getting dropped on death and wardrobe sync issues on death.

--- src/main/java/moe/plushie/armourers_workshop/common/handler/CapabilitySyncHandler.java
+++ src/main/java/moe/plushie/armourers_workshop/common/handler/CapabilitySyncHandler.java
@@ -6,14 +6,22 @@ import moe.plushie.armourers_workshop.common.capability.wardrobe.IWardrobeCap;
 import moe.plushie.armourers_workshop.common.capability.wardrobe.WardrobeCap;
 import moe.plushie.armourers_workshop.common.capability.wardrobe.player.IPlayerWardrobeCap;
 import moe.plushie.armourers_workshop.common.capability.wardrobe.player.PlayerWardrobeCap;
+import moe.plushie.armourers_workshop.common.config.ConfigHandler;
 import moe.plushie.armourers_workshop.common.lib.LibModInfo;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability.IStorage;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
 
 @Mod.EventBusSubscriber(modid = LibModInfo.ID)
 public final class CapabilitySyncHandler {
@@ -52,9 +60,13 @@ public final class CapabilitySyncHandler {
         }
     }
     
-    /* @SubscribeEvent
-    public static void onLivingDeathEvent (LivingDeathEvent  event) {
-        if (!event.getEntity().getEntityWorld().isRemote && event.getEntity() instanceof EntityPlayerMP) {
+    @SubscribeEvent
+    public static void onLivingDeathEvent(LivingDeathEvent  event) {
+        if (!event.getEntity().getEntityWorld().isRemote & event.getEntityLiving() instanceof EntityPlayer) {
+            IEntitySkinCapability skinCapability = EntitySkinCapability.get(event.getEntityLiving());
+            if (skinCapability == null) {
+                return;
+            }
             boolean dropSkins = true;
             MinecraftServer server = event.getEntity().getEntityWorld().getMinecraftServer();
             GameRules gr = getGameRules(server);
@@ -63,7 +75,7 @@ public final class CapabilitySyncHandler {
                 keepInventory = gr.getBoolean("keepInventory");
             }
             
-            switch (ConfigHandler.dropSkinsOnDeath) {
+            switch (ConfigHandler.wardrobeDropSkinsOnDeath) {
             case 0:
                 dropSkins = !keepInventory;
                 break;
@@ -77,10 +89,8 @@ public final class CapabilitySyncHandler {
                 dropSkins = !keepInventory;
                 break;
             }
-
-            ExPropsPlayerSkinData playerData = ExPropsPlayerSkinData.get((EntityPlayer) event.getEntity());
             if (dropSkins) {
-                playerData.getWardrobeInventoryContainer().dropItems((EntityPlayer) event.getEntity());
+                skinCapability.getSkinInventoryContainer().dropItems((EntityPlayer) event.getEntityLiving());
             }
         }
     }
@@ -90,11 +100,34 @@ public final class CapabilitySyncHandler {
     }
     
     @SubscribeEvent
-    public static void onLivingDeathEvent (PlayerEvent.Clone  event) {
-        NBTTagCompound compound = new NBTTagCompound();
-        ExPropsPlayerSkinData oldProps = ExPropsPlayerSkinData.get(event.getOriginal());
-        ExPropsPlayerSkinData newProps = ExPropsPlayerSkinData.get(event.getEntityPlayer());
-        oldProps.saveNBTData(compound);
-        newProps.loadNBTData(compound);
-    }*/
+    public static void onRespawn(PlayerRespawnEvent event) {
+        if (!event.isEndConquered()) {
+            IPlayerWardrobeCap wardrobeCap = PlayerWardrobeCap.get(event.player);
+            wardrobeCap.syncToAllTracking();
+            wardrobeCap.syncToPlayer((EntityPlayerMP) event.player);
+            
+            IEntitySkinCapability skinCap = EntitySkinCapability.get(event.player);
+            skinCap.syncToAllTracking();
+            skinCap.syncToPlayer((EntityPlayerMP) event.player);
+        }
+    }
+    
+    @SubscribeEvent
+    public static void onPlayerClone(PlayerEvent.Clone event) {
+        World world = event.getEntityPlayer().getEntityWorld();
+        if (event.isWasDeath()) {
+            NBTBase nbt = null;
+            IPlayerWardrobeCap wardrobeCapOld = PlayerWardrobeCap.get(event.getOriginal());
+            IPlayerWardrobeCap wardrobeCapNew = PlayerWardrobeCap.get(event.getEntityPlayer());
+            IStorage<IPlayerWardrobeCap> storageWardrobe = PlayerWardrobeCap.PLAYER_WARDROBE_CAP.getStorage();
+            nbt = storageWardrobe.writeNBT(PlayerWardrobeCap.PLAYER_WARDROBE_CAP, wardrobeCapOld, null);
+            storageWardrobe.readNBT(PlayerWardrobeCap.PLAYER_WARDROBE_CAP, wardrobeCapNew, null, nbt);
+            
+            IEntitySkinCapability skinCapOld = EntitySkinCapability.get(event.getOriginal());
+            IEntitySkinCapability skinCapNew = EntitySkinCapability.get(event.getEntityPlayer());
+            IStorage<IEntitySkinCapability> storageEntitySkin = EntitySkinCapability.ENTITY_SKIN_CAP.getStorage();
+            nbt = storageEntitySkin.writeNBT(EntitySkinCapability.ENTITY_SKIN_CAP, skinCapOld, null);
+            storageEntitySkin.readNBT(EntitySkinCapability.ENTITY_SKIN_CAP, skinCapNew, null, nbt);
+        }
+    }
 }
--- src/main/java/moe/plushie/armourers_workshop/common/inventory/WardrobeInventory.java
+++ src/main/java/moe/plushie/armourers_workshop/common/inventory/WardrobeInventory.java
@@ -2,11 +2,10 @@ package moe.plushie.armourers_workshop.common.inventory;
 
 import moe.plushie.armourers_workshop.api.common.skin.type.ISkinType;
 import moe.plushie.armourers_workshop.utils.NBTHelper;
-import net.minecraft.entity.item.EntityItem;
+import moe.plushie.armourers_workshop.utils.UtilItems;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.World;
 
 public class WardrobeInventory extends ModInventory {
     
@@ -36,20 +35,11 @@ public class WardrobeInventory extends ModInventory {
     }
     
     public void dropItems(EntityPlayer player) {
-        World world = player.getEntityWorld();
-        double x = player.posX;
-        double y = player.posY;
-        double z = player.posZ;
         for (int i = 0; i < getSizeInventory(); i++) {
             ItemStack stack = getStackInSlot(i);
-            if (stack != null) {
-                float f = 0.7F;
-                double xV = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
-                double yV = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
-                double zV = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
-                EntityItem entityitem = new EntityItem(world, (double)x + xV, (double)y + yV, (double)z + zV, stack);
-                world.spawnEntity(entityitem);
-                setInventorySlotContents(i, null);
+            if (!stack.isEmpty()) {
+                UtilItems.spawnItemAtEntity(player, stack, false);
+                setInventorySlotContents(i, ItemStack.EMPTY);
             }
         }
     }
