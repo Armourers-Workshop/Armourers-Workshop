4529f6d7a34b0fb7614ce46aea3fa2b833f54ccc
RiskyKen <RiskyKen@users.noreply.github.com>
2018-11-05 21:26:01 +0000

    Fixed global skin library TESR not rotation with the block.

--- src/main/java/moe/plushie/armourers_workshop/client/render/tileEntity/RenderBlockGlobalSkinLibrary.java
+++ src/main/java/moe/plushie/armourers_workshop/client/render/tileEntity/RenderBlockGlobalSkinLibrary.java
@@ -4,9 +4,13 @@ import org.lwjgl.opengl.GL11;
 
 import moe.plushie.armourers_workshop.client.config.ConfigHandlerClient;
 import moe.plushie.armourers_workshop.client.model.block.ModelBlockGlobalSkinLibrary;
+import moe.plushie.armourers_workshop.common.blocks.BlockGlobalSkinLibrary;
+import moe.plushie.armourers_workshop.common.blocks.ModBlocks;
 import moe.plushie.armourers_workshop.common.tileentities.TileEntityGlobalSkinLibrary;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.EnumFacing;
 
 public class RenderBlockGlobalSkinLibrary extends TileEntitySpecialRenderer<TileEntityGlobalSkinLibrary> {
 
@@ -22,11 +26,19 @@ public class RenderBlockGlobalSkinLibrary extends TileEntitySpecialRenderer<Tile
         if (ConfigHandlerClient.useClassicBlockModels) {
             shrink = 1;
         } else {
-            GlStateManager.translate(3.5F * SCALE, -2 * SCALE, 5F * SCALE);
+            float xPos = 2.5F;
+            float yPos = 4.6F;
+            IBlockState state = te.getWorld().getBlockState(te.getPos());
+            if (state.getBlock() == ModBlocks.globalSkinLibrary) {
+                EnumFacing facing = state.getValue(BlockGlobalSkinLibrary.STATE_FACING);
+                GlStateManager.translate(
+                        (xPos * SCALE * facing.getZOffset()) + (yPos * SCALE * -facing.getXOffset()),
+                        4 * SCALE,
+                        (xPos * SCALE * facing.getXOffset() + (yPos * SCALE * facing.getZOffset()))
+                        );
+            }
         }
-        
         GlStateManager.scale(shrink, shrink, shrink);
-        
         GLOBE_MODEL.render(te, partialTicks, SCALE);
         GL11.glPopMatrix();
         super.render(te, x, y, z, partialTicks, destroyStage, alpha);
