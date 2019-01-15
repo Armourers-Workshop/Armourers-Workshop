74cb5e7130d84c37adf59d21e38c47dc401ca05b
RiskyKen <RiskyKen@users.noreply.github.com>
2019-01-15 14:22:15 +0000

    Fixed skin paint not being mirrored on left limbs.

--- src/main/java/moe/plushie/armourers_workshop/client/render/EntityTextureInfo.java
+++ src/main/java/moe/plushie/armourers_workshop/client/render/EntityTextureInfo.java
@@ -248,12 +248,56 @@ public class EntityTextureInfo {
         texture.setRGB(x, y, rgb);
         // Paint left leg.
         if (x < 16 & y >= 16 & y < 32) {
-            texture.setRGB(x + 16, y + 32, rgb);
+            if (y >= 20) {
+                if (x < 12) {
+                    texture.setRGB(15 + (12 - x), y + 32, rgb);
+                } else {
+                    // Back face
+                    texture.setRGB(27 + (4 - (x - 12)), y + 32, rgb);
+                }
+            } else {
+                // Top and bottom
+                if (x < 8) {
+                    texture.setRGB(15 + (8 - (x - 4)), y + 32, rgb);
+                } else {
+                    texture.setRGB(23 + (8 - (x - 4)), y + 32, rgb);
+                }
+                
+            }
         }
 
-        // Paint right arm.
+        // Paint left arm.
         if (x >= 40 & x < 56 & y >= 16 & y < 32) {
-            texture.setRGB(x - 8, y + 32, rgb);
+            if (y >= 20) {
+                if (x < 52) {
+                    texture.setRGB(12 - (x - 40) + 31, y + 32, rgb);
+                } else {
+                    // Back face
+                    texture.setRGB(4 - (x - 52) + 43, y + 32, rgb);
+                }
+            } else {
+                // Top and bottom
+                if (x < 48) {
+                    texture.setRGB((8 - (x - 40) + 4) + 31, y + 32, rgb);
+                } else {
+                    texture.setRGB((8 - (x - 48) + 4) + 31, y + 32, rgb);
+                }
+                
+            }
+        }
+    }
+    
+    private void paintMirroredPart(BufferedImage texture, int x, int y, int rgb, int width, int height, int depth, int offsetX, int offsetY) {
+        if (y >= 20) {
+            if (x < 12) {
+                texture.setRGB(15 + (12 - x), y + 32, rgb);
+            } else {
+                // Back face
+                texture.setRGB(27 + (4 - (x - 12)), y + 32, rgb);
+            }
+        } else {
+            // Top and bottom
+            texture.setRGB(15 + (16 - x), y + 32, rgb);
         }
     }
 
