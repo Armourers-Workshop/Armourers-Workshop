934a3b972dac0975d6f930952051c17c34183d21
RiskyKen <RiskyKen@users.noreply.github.com>
2019-04-10 18:18:49 +0100

    Changed release type to alpha.

--- src/main/java/moe/plushie/armourers_workshop/common/lib/LibModInfo.java
+++ src/main/java/moe/plushie/armourers_workshop/common/lib/LibModInfo.java
@@ -6,6 +6,7 @@ public class LibModInfo {
     public static final String NAME = "Armourer's Workshop";
     public static final String MOD_VERSION = "@VERSION@";
     //public static final String MOD_VERSION = "1.12.2-0.49.0.397";
+    public static final ReleaseType RELEASE_TYPE = ReleaseType.ALPHA;
     public static final String MC_VERSION = "1.12.2";
     public static final String CHANNEL = "arms-ws";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.3.2694,);after:galacticraftcore;";
@@ -16,4 +17,12 @@ public class LibModInfo {
     public static final String PROXY_CLIENT_CLASS = "moe.plushie.armourers_workshop.proxies.ClientProxy";
     public static final String PROXY_COMMNON_CLASS = "moe.plushie.armourers_workshop.proxies.CommonProxy";
     public static final String GUI_FACTORY_CLASS = "moe.plushie.armourers_workshop.client.gui.config.ModGuiFactory";
+    
+    public static enum ReleaseType {
+        PRE_APLHA,
+        ALPHA,
+        BETA,
+        RC,
+        RELEASED
+    }
 }
