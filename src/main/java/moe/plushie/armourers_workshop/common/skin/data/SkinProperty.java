402c18fc4d6a2d292588053e1214844721924425
RiskyKen <RiskyKen@users.noreply.github.com>
2019-07-12 13:13:21 +0100

    Fixed some old outfits not loading wing data correctly.

--- src/main/java/moe/plushie/armourers_workshop/common/skin/data/SkinProperty.java
+++ src/main/java/moe/plushie/armourers_workshop/common/skin/data/SkinProperty.java
@@ -3,43 +3,49 @@ package moe.plushie.armourers_workshop.common.skin.data;
 import moe.plushie.armourers_workshop.api.common.skin.data.ISkinProperty;
 
 public class SkinProperty<T> implements ISkinProperty<T> {
-    
+
     private String key;
     private T defaultValue;
-    
+
     public SkinProperty(String key, T defaultValue) {
         this.key = key;
         this.defaultValue = defaultValue;
     }
-    
+
     public T getDefaultValue() {
         return defaultValue;
     }
-    
+
     public String getKey() {
         return key;
     }
-    
+
     public T getValue(SkinProperties properties) {
         return (T) properties.getProperty(key, defaultValue);
     }
-    
+
     public void setValue(SkinProperties properties, T value) {
         properties.setProperty(key, value);
     }
-    
+
     public void clearValue(SkinProperties properties) {
         properties.removeProperty(key);
     }
-    
+
     public T getValue(SkinProperties properties, int index) {
-        return (T) properties.getProperty(key + String.valueOf(index), defaultValue);
+        if (properties.haveProperty(key + String.valueOf(index))) {
+            return (T) properties.getProperty(key + String.valueOf(index), defaultValue);
+        } else if (properties.haveProperty(key)) {
+            return (T) properties.getProperty(key, defaultValue);
+        } else {
+            return defaultValue;
+        }
     }
-    
+
     public void setValue(SkinProperties properties, T value, int index) {
         properties.setProperty(key + String.valueOf(index), value);
     }
-    
+
     public void clearValue(SkinProperties properties, int index) {
         properties.removeProperty(key + String.valueOf(index));
     }
