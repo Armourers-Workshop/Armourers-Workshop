b0e53c31d28394c3cf945ef73dffce8b795d55c8
RiskyKen <RiskyKen@users.noreply.github.com>
2019-07-16 19:07:28 +0100

    Draw skin type in global library preview panels.

--- src/main/java/riskyken/armourersWorkshop/client/gui/controls/GuiControlSkinPanel.java
+++ src/main/java/riskyken/armourersWorkshop/client/gui/controls/GuiControlSkinPanel.java
@@ -14,6 +14,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import riskyken.armourersWorkshop.client.render.ModRenderHelper;
@@ -28,7 +30,7 @@ import riskyken.armourersWorkshop.common.skin.data.SkinPointer;
 public class GuiControlSkinPanel extends GuiButtonExt {
 
     private static final ResourceLocation TEXTURE = new ResourceLocation(LibModInfo.ID.toLowerCase(), "textures/gui/controls/skin-panel.png");
-    
+
     private final ArrayList<SkinIcon> iconList;
     private int panelPadding;
     private int iconPadding;
@@ -39,16 +41,16 @@ public class GuiControlSkinPanel extends GuiButtonExt {
     private int colCount;
     private boolean showName;
     private SkinIcon lastPressedSkinIcon;
-    
+
     public GuiControlSkinPanel() {
         this(0, 0, 0, 0);
     }
-    
+
     public GuiControlSkinPanel(int xPos, int yPos, int width, int height) {
         super(0, xPos, yPos, width, height, "");
         iconList = new ArrayList<SkinIcon>();
     }
-    
+
     public void init(int x, int y, int width, int height) {
         this.xPosition = x;
         this.yPosition = y;
@@ -61,45 +63,45 @@ public class GuiControlSkinPanel extends GuiButtonExt {
         this.lastPressedSkinIcon = null;
         updateIconCount();
     }
-    
+
     public void setIconSize(int iconSize) {
         this.iconSize = iconSize;
         updateIconCount();
     }
-    
+
     public void setPanelPadding(int panelPadding) {
         this.panelPadding = panelPadding;
     }
-    
+
     public void setShowName(boolean showName) {
         this.showName = showName;
     }
-    
+
     public void updateIconCount() {
         int boxW = width + iconPadding - panelPadding * 2;
         int boxH = height + iconPadding - panelPadding * 2;
-        rowCount = Math.max(1, (int) Math.floor(boxW / (iconSize + iconPadding))) ;
+        rowCount = Math.max(1, (int) Math.floor(boxW / (iconSize + iconPadding)));
         colCount = Math.max(1, (int) Math.floor(boxH / (iconSize + iconPadding)));
         iconCount = rowCount * colCount;
     }
-    
+
     public int getIconCount() {
         return iconCount;
     }
-    
+
     @Override
     public void drawButton(Minecraft mc, int mouseX, int mouseY) {
         if (visible) {
             this.field_146123_n = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
             int hover = this.getHoverState(this.field_146123_n);
             drawGradientRect(xPosition, yPosition, xPosition + this.width, yPosition + height, 0xC0222222, 0xD0333333);
-            
+
             for (int i = 0; i < iconList.size(); i++) {
                 int x = i % rowCount;
                 int y = (int) (i / rowCount);
                 int iconX = xPosition + x * (iconSize + iconPadding) + panelPadding;
                 int iconY = yPosition + y * (iconSize + iconPadding) + panelPadding;
-                
+
                 SkinIcon skinIcon = iconList.get(i);
                 if (y < colCount) {
                     skinIcon.drawIcon(iconX, iconY, mouseX, mouseY, iconSize, showName);
@@ -115,7 +117,7 @@ public class GuiControlSkinPanel extends GuiButtonExt {
             int y = (int) (i / rowCount);
             int iconX = xPosition + x * (iconSize + iconPadding) + panelPadding;
             int iconY = yPosition + y * (iconSize + iconPadding) + panelPadding;
-            
+
             SkinIcon skinIcon = iconList.get(i);
             if (y < colCount) {
                 if (skinIcon.mouseOver(iconX, iconY, mouseX, mouseY, iconSize)) {
@@ -126,11 +128,11 @@ public class GuiControlSkinPanel extends GuiButtonExt {
         }
         return false;
     }
-    
+
     public SkinIcon getLastPressedSkinIcon() {
         return lastPressedSkinIcon;
     }
-    
+
     public void clearIcons() {
         iconList.clear();
     }
@@ -138,21 +140,21 @@ public class GuiControlSkinPanel extends GuiButtonExt {
     public void addIcon(JsonObject skinJson) {
         iconList.add(new SkinIcon(skinJson));
     }
-    
+
     public class SkinIcon {
-        
+
         private final JsonObject skinJson;
         private final int id;
-        
+
         public SkinIcon(JsonObject skinJson) {
             this.skinJson = skinJson;
             id = skinJson.get("id").getAsInt();
         }
-        
+
         public JsonObject getSkinJson() {
             return skinJson;
         }
-        
+
         public void drawIcon(int x, int y, int mouseX, int mouseY, int iconSize, boolean showName) {
             if (mouseOver(x, y, mouseX, mouseY, iconSize)) {
                 drawRect(x, y, x + iconSize, y + iconSize, 0xC0777711);
@@ -167,15 +169,26 @@ public class GuiControlSkinPanel extends GuiButtonExt {
                     String name = skinJson.get("name").getAsString();
                     FontRenderer fontRenderer = Minecraft.getMinecraft().fontRenderer;
                     int size = fontRenderer.getStringWidth(skin.getCustomName());
-                    
+
                     List list = fontRenderer.listFormattedStringToWidth(name, iconSize - 2);
-                    
+
                     int textY = y + iconSize;
                     textY -= fontRenderer.FONT_HEIGHT * list.size();
-                    
+
                     fontRenderer.drawSplitString(name, x + 1, textY, iconSize - 2, 0xFFEEEEEE);
                 }
-                
+
+                IIcon skinIcon = skin.getSkinType().getIcon();
+                if (skinIcon != null) {
+                    GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                    Minecraft mc = Minecraft.getMinecraft();
+                    // GL11.glDisable(GL11.GL_DEPTH_TEST);
+                    GL11.glDisable(GL11.GL_LIGHTING);
+                    GL11.glEnable(GL11.GL_BLEND);
+                    mc.getTextureManager().bindTexture(TextureMap.locationItemsTexture);
+                    drawTexturedModelRectFromIcon(x, y, skinIcon, 10, 10);
+                }
+
                 float scale = iconSize / 2;
                 GL11.glPushMatrix();
                 GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
@@ -184,13 +197,13 @@ public class GuiControlSkinPanel extends GuiButtonExt {
                 } else {
                     GL11.glTranslatef(x + iconSize / 2, y + iconSize / 2, 200.0F);
                 }
-                GL11.glScalef((float)(-10), (float)10, (float)10);
+                GL11.glScalef((float) (-10), (float) 10, (float) 10);
                 if (mouseOver(x, y, mouseX, mouseY, iconSize)) {
                     GL11.glScalef(1.5F, 1.5F, 1.5F);
                 }
                 GL11.glRotatef(180.0F, 0.0F, 1.0F, 0.0F);
                 GL11.glRotatef(20.0F, 1.0F, 0.0F, 0.0F);
-                float rotation = (float)((double)System.currentTimeMillis() / 10 % 360);
+                float rotation = (float) ((double) System.currentTimeMillis() / 10 % 360);
                 GL11.glRotatef(rotation, 0.0F, 1.0F, 0.0F);
                 RenderHelper.enableStandardItemLighting();
                 GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
@@ -198,29 +211,23 @@ public class GuiControlSkinPanel extends GuiButtonExt {
                 GL11.glEnable(GL11.GL_COLOR_MATERIAL);
                 ModRenderHelper.enableAlphaBlend();
                 SkinItemRenderHelper.renderSkinAsItem(skin, new SkinPointer(skin), true, false, iconSize, iconSize);
-                //ItemStackRenderHelper.renderItemModelFromSkin(skin, new SkinPointer(skin), true, false);
+                // ItemStackRenderHelper.renderItemModelFromSkin(skin, new SkinPointer(skin), true, false);
                 GL11.glPopAttrib();
                 GL11.glPopMatrix();
-                
+
             } else {
                 Minecraft.getMinecraft().renderEngine.bindTexture(TEXTURE);
                 int speed = 60;
                 int frames = 18;
-                
-                int frame = (int) ((System.currentTimeMillis() / (long)speed) % frames);
+
+                int frame = (int) ((System.currentTimeMillis() / (long) speed) % frames);
                 int u = MathHelper.floor_double(frame / 9);
                 int v = frame - u * 9;
-                
-                Gui.func_152125_a(
-                        x + 8, y + 8,
-                        u * 28, v * 28,
-                        27, 27,
-                        iconSize - 16, iconSize - 16,
-                        256, 256
-                        );
+
+                Gui.func_152125_a(x + 8, y + 8, u * 28, v * 28, 27, 27, iconSize - 16, iconSize - 16, 256, 256);
             }
         }
-        
+
         public boolean mouseOver(int x, int y, int mouseX, int mouseY, int iconSize) {
             return mouseX >= x & mouseY >= y & mouseX < x + iconSize & mouseY < y + iconSize;
         }
