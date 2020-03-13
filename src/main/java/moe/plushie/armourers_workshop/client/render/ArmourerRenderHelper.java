cca0bc80b304724ea8932bec3d59795c0a7d2507
RiskyKen <RiskyKen@users.noreply.github.com>
2020-03-13 22:07:20 +0000

    Fixed bottom armourer grid not always rendering.

--- src/main/java/moe/plushie/armourers_workshop/client/render/ArmourerRenderHelper.java
+++ src/main/java/moe/plushie/armourers_workshop/client/render/ArmourerRenderHelper.java
@@ -25,9 +25,9 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public final class ArmourerRenderHelper {
-    
+
     private static final ResourceLocation guideImage = new ResourceLocation(LibModInfo.ID.toLowerCase(), "textures/blocks/guide.png");
-    
+
     public static void renderBuildingGuide(ISkinType skinType, float scale, SkinProperties skinProps, boolean showHelper) {
         for (int i = 0; i < skinType.getSkinParts().size(); i++) {
             ISkinPartType skinPart = skinType.getSkinParts().get(i);
@@ -38,10 +38,10 @@ public final class ArmourerRenderHelper {
             GlStateManager.translate(partOffset.getX() * scale, partOffset.getY() * scale, partOffset.getZ() * scale);
             skinPart.renderBuildingGuide(scale, skinProps, showHelper);
             GlStateManager.translate(-partOffset.getX() * scale, -partOffset.getY() * scale, -partOffset.getZ() * scale);
-            
+
         }
     }
-    
+
     public static void renderBuildingGrid(ISkinType skinType, float scale, boolean showGuides, SkinProperties skinProps, boolean multiblock) {
         for (int i = 0; i < skinType.getSkinParts().size(); i++) {
             ISkinPartType skinPartType = skinType.getSkinParts().get(i);
@@ -49,23 +49,23 @@ public final class ArmourerRenderHelper {
             GlStateManager.translate(partOffset.getX() * scale, partOffset.getY() * scale, partOffset.getZ() * scale);
             if (skinType == SkinTypeRegistry.skinBlock) {
                 if (skinPartType.getPartName().equals("multiblock") & multiblock) {
-                    //GL11.glColor4f(1F, 1F, 0.0F, 0.2F);
-                    renderBuildingGrid(((SkinBlock)SkinTypeRegistry.skinBlock).partBase, scale, showGuides, skinProps, 1F, 1F, 0.0F, 0.2F);
+                    // GL11.glColor4f(1F, 1F, 0.0F, 0.2F);
+                    renderBuildingGrid(((SkinBlock) SkinTypeRegistry.skinBlock).partBase, scale, showGuides, skinProps, 1F, 1F, 0.0F, 0.2F);
                     GL11.glPolygonOffset(6F, 6F);
-                    //GL11.glColor4f(0.5F, 0.5F, 0.5F, 0.25F);
+                    // GL11.glColor4f(0.5F, 0.5F, 0.5F, 0.25F);
                     renderBuildingGrid(skinPartType, scale, showGuides, skinProps, 0.5F, 0.5F, 0.5F, 0.25F);
                 } else if (skinPartType.getPartName().equals("base") & !multiblock) {
-                    //GL11.glColor4f(0.5F, 0.5F, 0.5F, 0.25F);
+                    // GL11.glColor4f(0.5F, 0.5F, 0.5F, 0.25F);
                     renderBuildingGrid(skinPartType, scale, showGuides, skinProps, 0.5F, 0.5F, 0.5F, 0.25F);
                 }
             } else {
-                //GL11.glColor4f(0.5F, 0.5F, 0.5F, 0.25F);
+                // GL11.glColor4f(0.5F, 0.5F, 0.5F, 0.25F);
                 renderBuildingGrid(skinPartType, scale, showGuides, skinProps, 0.5F, 0.5F, 0.5F, 0.25F);
             }
             GlStateManager.translate(-partOffset.getX() * scale, -partOffset.getY() * scale, -partOffset.getZ() * scale);
-        }  
+        }
     }
-    
+
     public static void renderBuildingGrid(ISkinPartType skinPartType, float scale, boolean showGuides, SkinProperties skinProps, float r, float g, float b, float a) {
         GlStateManager.translate(0, skinPartType.getBuildingSpace().getY() * scale, 0);
         GlStateManager.scale(-1, -1, 1);
@@ -73,46 +73,42 @@ public final class ArmourerRenderHelper {
         GlStateManager.scale(-1, -1, 1);
         GlStateManager.translate(0, -skinPartType.getBuildingSpace().getY() * scale, 0);
     }
-    
+
     private static void renderGuidePart(ISkinPartType part, float scale, boolean showGuides, SkinProperties skinProps, float r, float g, float b, float a) {
         Minecraft.getMinecraft().getTextureManager().bindTexture(guideImage);
-        
+
         IRectangle3D buildRec = part.getBuildingSpace();
         IRectangle3D guideRec = part.getGuideSpace();
-        
+
         GlStateManager.pushMatrix();
-        //GL11.glDisable(GL11.GL_LIGHTING);
-        
+        // GL11.glDisable(GL11.GL_LIGHTING);
+
         if (showGuides) {
-            //render building grid
+            // render building grid
             //
-            renderGuideBox(buildRec.getX(), buildRec.getY(), buildRec.getZ(),
-                    buildRec.getWidth(), buildRec.getHeight(), buildRec.getDepth(), scale, r, g, b, a);
-            //render origin
-            //GL11.glColor4f(0F, 1F, 0F, 0.5F);
+            renderGuideBox(buildRec.getX(), buildRec.getY(), buildRec.getZ(), buildRec.getWidth(), buildRec.getHeight(), buildRec.getDepth(), scale, r, g, b, a);
+            // render origin
+            // GL11.glColor4f(0F, 1F, 0F, 0.5F);
             renderGuideBox(-0.5F, -0.5F, -0.5F, 1, 1, 1, scale, 0F, 1F, 0F, 0.5F);
         }
 
-        
         if (ConfigHandlerClient.showArmourerDebugRender) {
-            //render debug box
-            //GL11.glColor4f(1F, 0F, 0F, 0.25F);
-            renderGuideBox(guideRec.getX(), guideRec.getY(), guideRec.getZ(),
-                    guideRec.getWidth(), guideRec.getHeight(), guideRec.getDepth(), scale, 1F, 0F, 0F, 0.25F);
+            // render debug box
+            // GL11.glColor4f(1F, 0F, 0F, 0.25F);
+            renderGuideBox(guideRec.getX(), guideRec.getY(), guideRec.getZ(), guideRec.getWidth(), guideRec.getHeight(), guideRec.getDepth(), scale, 1F, 0F, 0F, 0.25F);
         }
 
         if (part.isModelOverridden(skinProps)) {
-            //GL11.glColor4f(0F, 0F, 1F, 0.25F);
-            renderGuideBox(guideRec.getX(), guideRec.getY(), guideRec.getZ(),
-                    guideRec.getWidth(), guideRec.getHeight(), guideRec.getDepth(), scale, 0F, 0F, 1F, 0.25F);
+            // GL11.glColor4f(0F, 0F, 1F, 0.25F);
+            renderGuideBox(guideRec.getX(), guideRec.getY(), guideRec.getZ(), guideRec.getWidth(), guideRec.getHeight(), guideRec.getDepth(), scale, 0F, 0F, 1F, 0.25F);
         }
-        
-        //GL11.glColor4f(1F, 1F, 1F, 1F);
-        
-        //GL11.glEnable(GL11.GL_LIGHTING);
+
+        // GL11.glColor4f(1F, 1F, 1F, 1F);
+
+        // GL11.glEnable(GL11.GL_LIGHTING);
         GlStateManager.popMatrix();
     }
-    
+
     private static void renderGuideBox(double x, double y, double z, int width, int height, int depth, float scale, float r, float g, float b, float a) {
         renderGuideFace(EnumFacing.DOWN, x, y, z, width, depth, scale, r, g, b, a);
         renderGuideFace(EnumFacing.UP, x, y + height, z, width, depth, scale, r, g, b, a);
@@ -121,22 +117,20 @@ public final class ArmourerRenderHelper {
         renderGuideFace(EnumFacing.NORTH, x, y, z, width, height, scale, r, g, b, a);
         renderGuideFace(EnumFacing.SOUTH, x, y, z + depth, width, height, scale, r, g, b, a);
     }
-    
+
     private static void renderGuideFace(EnumFacing dir, double x, double y, double z, double sizeX, double sizeY, float scale, float r, float g, float b, float a) {
         Tessellator tessellator = Tessellator.getInstance();
-        
+
         GlStateManager.pushMatrix();
         GlStateManager.pushAttrib();
 
-        
         float scale1 = 0.999F;
-        //GL11.glScalef(scale1, scale1, scale1);
+        // GL11.glScalef(scale1, scale1, scale1);
         GlStateManager.translate(x * scale, y * scale, z * scale);
-        
-        //GlStateManager.resetColor();
-        //GlStateManager.color(1F, 1F, 1F, 1F);
-        
-        
+
+        // GlStateManager.resetColor();
+        // GlStateManager.color(1F, 1F, 1F, 1F);
+
         switch (dir) {
         case EAST:
             GlStateManager.rotate(-90, 0, 1, 0);
@@ -153,43 +147,33 @@ public final class ArmourerRenderHelper {
         default:
             break;
         }
-        
+
+        GlStateManager.enableCull();
         GlStateManager.disableCull();
         GlStateManager.enableBlend();
         GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
-        
+
         Tessellator tess = Tessellator.getInstance();
         BufferBuilder buff = tess.getBuffer();
         buff.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
-        
-        buff.pos(0, 0, 0)
-        .tex(0, 0)
-        .color(r,  g, b, a)
-        .endVertex();
-        
-        buff.pos(0, sizeY * scale, 0)
-        .tex(sizeY, 0)
-        .color(r, g, b, a)
-        .endVertex();
-        
-        buff.pos(sizeX * scale, sizeY * scale, 0)
-        .tex(sizeY, sizeX)
-        .color(r, g, b, a)
-        .endVertex();
-        
-        buff.pos(sizeX * scale, 0, 0)
-        .tex(0, sizeX)
-        .color(r, g, b, a)
-        .endVertex();
-        
+
+        buff.pos(0, 0, 0).tex(0, 0).color(r, g, b, a).endVertex();
+
+        buff.pos(0, sizeY * scale, 0).tex(sizeY, 0).color(r, g, b, a).endVertex();
+
+        buff.pos(sizeX * scale, sizeY * scale, 0).tex(sizeY, sizeX).color(r, g, b, a).endVertex();
+
+        buff.pos(sizeX * scale, 0, 0).tex(0, sizeX).color(r, g, b, a).endVertex();
+
         tess.draw();
-        
+
         ModRenderHelper.disableAlphaBlend();
         GlStateManager.disableBlend();
-        //GlStateManager.blendFunc(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
-        
+        // GlStateManager.blendFunc(SourceFactor.SRC_ALPHA,
+        // DestFactor.ONE_MINUS_SRC_ALPHA);
+
         GlStateManager.enableCull();
-        
+
         GlStateManager.popAttrib();
         GlStateManager.popMatrix();
     }
