1b9a1314ad066ce4b3fc38d0e14dd1a065488f63
SAGESSE <gdmmyzc@163.com>
2022-07-01 19:40:25 +0800

    Fix wings build origin too height

--- src/main/java/moe/plushie/armourers_workshop/core/skin/part/wings/RightWingPartType.java
+++ src/main/java/moe/plushie/armourers_workshop/core/skin/part/wings/RightWingPartType.java
@@ -9,7 +9,7 @@ public class RightWingPartType extends SkinPartType implements ICanRotation {
 
     public RightWingPartType() {
         super();
-        this.buildingSpace = new Rectangle3i(0, -32, -28, 48, 64, 64);
+        this.buildingSpace = new Rectangle3i(0, -27, -28, 48, 64, 64);
         this.guideSpace = new Rectangle3i(-4, -12, -4, 8, 12, 4);
         this.offset = new Vector3i(0, -1, 2);
     }
