d11497d51eef87d30acf18becbb30faabd33d177
SAGESSE <gdmmyzc@163.com>
2022-12-08 23:30:18 +0800

    Refactoring the packets to use fixed packet id

--- forge/src/main/java/moe/plushie/armourers_workshop/init/platform/forge/addon/BukkitAddon.java
+++ forge/src/main/java/moe/plushie/armourers_workshop/init/platform/forge/addon/BukkitAddon.java
@@ -1,5 +1,8 @@
 package moe.plushie.armourers_workshop.init.platform.forge.addon;
 
+import moe.plushie.armourers_workshop.init.ModLog;
+import moe.plushie.armourers_workshop.init.environment.EnvironmentExecutor;
+import moe.plushie.armourers_workshop.init.environment.EnvironmentType;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.ToIntFunction;
@@ -10,5 +13,8 @@ public class BukkitAddon {
     }
 
     public static void register(String category, ResourceLocation registryName, ToIntFunction<ResourceLocation> provider) {
+        EnvironmentExecutor.didSetup(EnvironmentType.COMMON, () -> () -> {
+            ModLog.debug("{} => {} => {}", category, registryName, provider.applyAsInt(registryName));
+        });
     }
 }
