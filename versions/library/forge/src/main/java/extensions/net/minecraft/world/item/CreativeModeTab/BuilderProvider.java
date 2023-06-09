ed76b6006eb5c7d172f9dd17bed05dcffa16221c
SAGESSE <gdmmyzc@163.com>
2023-06-09 10:58:20 +0800

    Fix CI issue on 1.20

--- versions/library/forge/src/main/java/extensions/net/minecraft/world/item/CreativeModeTab/BuilderProvider.java
+++ versions/library/forge/src/main/java/extensions/net/minecraft/world/item/CreativeModeTab/BuilderProvider.java
@@ -10,7 +10,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
