923929acadb8968534fff5762a192a25947ab11f
SAGESSE <gdmmyzc@163.com>
2023-07-04 16:22:17 +0800

    Fix old item skin height compatible issue in armourer

--- common/src/main/java/moe/plushie/armourers_workshop/core/skin/part/item/ItemPartType.java
+++ common/src/main/java/moe/plushie/armourers_workshop/core/skin/part/item/ItemPartType.java
@@ -9,7 +9,7 @@ public class ItemPartType extends SkinPartType implements ICanHeld {
 
     public ItemPartType() {
         super();
-        this.buildingSpace = new Rectangle3i(-32, -24, -32, 64, 72, 64);
+        this.buildingSpace = new Rectangle3i(-32, -24, -32, 64, 88, 64);
         this.guideSpace = new Rectangle3i(-2, -2, 2, 4, 4, 8);
         //Offset -1 to match old skin system.
         this.offset = new Vector3i(0, -1, 0);
