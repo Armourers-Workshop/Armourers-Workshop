a56e531f10c57320904733a42379e2b372b377f6
SAGESSE <gdmmyzc@163.com>
2026-05-19 16:16:12 +0800

    Fix Sophisticated Backpacks 3.24.38/3.25.29 compatible issue

--- versions/library/forge/src/main/java/moe/plushie/armourers_workshop/compat/forge/event/client/AbstractForgeItemTooltipEventImpl.java
+++ versions/library/forge/src/main/java/moe/plushie/armourers_workshop/compat/forge/event/client/AbstractForgeItemTooltipEventImpl.java
@@ -33,30 +33,4 @@ public class AbstractForgeItemTooltipEventImpl {
         }
         return new CGRect(j2, k2, i, j);
     }
-
-//                    var font = event.getFont();
-//                    var tooltips = event.getComponents();
-//                    int mouseX = event.getX();
-//                    int mouseY = event.getY();
-//                    int screenWidth = event.getScreenWidth();
-//                    int screenHeight = event.getScreenHeight();
-//                    int i = 0;
-//                    int j = tooltips.size() == 1 ? -2 : 0;
-//                    for (var tooltip : tooltips) {
-//                        int k = tooltip.getWidth(font);
-//                        if (k > i) {
-//                            i = k;
-//                        }
-//                        j += tooltip.getHeight();
-//                    }
-//                    int j2 = mouseX + 12;
-//                    int k2 = mouseY - 12;
-//                    if (j2 + i > screenWidth) {
-//                        j2 -= 28 + i;
-//                    }
-//                    if (k2 + j + 6 > screenHeight) {
-//                        k2 = screenHeight - j - 6;
-//                    }
-//                    return new CGRect(j2, k2, i, j);
-//
 }
