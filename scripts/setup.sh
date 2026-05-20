27c2b3ceb60e99cefa5769444d61518186dbc3d4
SAGESSE <gdmmyzc@163.com>
2026-05-20 13:58:20 +0800

    Add build artifacts for CI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,25 +2,26 @@
 set -e
 
 release_version="${1:-2.0.0-homebaked}"
-minecraft_versions="${2:-21.1}"
+minecraft_versions="${2:-..}"
 
-# generate game version.
-cat "versions/$minecraft_versions/gradle.properties" \
-  | sed -n 's/^minecraft_api_version=//p' \
-  | sed 's/^/game_version=/'
+# calculate versions.
+version="$(echo "$release_version" | cut -d'v' -f2)"
+game_version="$(cat "versions/$minecraft_versions/gradle.properties" | sed -n 's/^minecraft_api_version=//p')"
 
-# generate build version.
-echo "$release_version" \
-  | cut -d'v' -f2 \
-  | sed 's/^/release_version=/'
+cache_dependencies="$(find versions -maxdepth 2 -type f -name gradle.properties -print0 | xargs -0 sed -n '/^[^_]*_api_version=/p' | sort)"
+
+# generate build outputs.
+echo "version=$version"
+echo "game_version=$game_version"
+
+# generate archives outputs.
+echo "forge_archives_name=armourersworkshop-forge-$game_version-$version"
+echo "fabric_archives_name=armourersworkshop-fabric-$game_version-$version"
+echo "forge_archives_version=forge-$game_version-$version"
+echo "fabric_archives_version=fabric-$game_version-$version"
 
 # generate primary cache key.
-find versions -maxdepth 2 -type f -name gradle.properties -print0 \
-  | xargs -0 sed -n '/^[^_]*_api_version=/p' \
-  | sort \
-  | sha256sum \
-  | cut -d' ' -f1 \
-  | sed 's/^/cache_key=build-cache-/'
+echo "cache_key=build-cache-$(echo "$cache_dependencies" | sha256sum | cut -d' ' -f1)"
 
 # generate primary cache paths.
 echo "cache_path<<EOF"
